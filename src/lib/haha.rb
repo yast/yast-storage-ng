db5353a15f35e0d2be0c810afb3f029d0f08e250
Arvin Schnell <aschnell@suse.de>
2015-11-17 16:59:24 +0100

    - init libstorage logging

--- src/lib/haha.rb
+++ src/lib/haha.rb
@@ -1,20 +1,50 @@
 
 require "yast"
+require "yastx"
 require "storage"
+
 
 class Haha
 
-  def initialize
-    environment = Storage::Environment.new(true)
-    @storage = Storage::Storage.new(environment)
 
-    probed = storage.probed()
-    print probed
-   
+  class MyLogger < ::Storage::Logger
+
+    def initialize()
+      super()
+    end
+
+    def write(level, component, filename, line, function, content)
+      Yast::y2_logger(level, component, filename, line, function, content)
+    end
+
   end
+
+
+  def initialize
+
+    @my_logger = MyLogger.new()
+    Storage::logger = @my_logger
+
+    @environment = Storage::Environment.new(true)
+
+    @storage = Storage::Storage.new(@environment)
+
+  end
+
 
   def storage
     return @storage
   end
+
+
+end
+
+
+if $PROGRAM_NAME == __FILE__
+
+  haha = Haha.new
+
+  probed = haha.storage.probed()
+  print probed
 
 end
