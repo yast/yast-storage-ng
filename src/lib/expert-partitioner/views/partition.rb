a6f5e44912416b87173f42857478a844b7809738
Arvin Schnell <aschnell@suse.de>
2015-11-27 17:34:44 +0100

    - show udev path and ids

--- src/lib/expert-partitioner/views/partition.rb
+++ src/lib/expert-partitioner/views/partition.rb
@@ -22,7 +22,13 @@ module ExpertPartitioner
     def create
 
       tmp = [ "Name: #{@partition.name}",
-              "Size: #{::Storage::byte_to_humanstring(1024 * @partition.size_k, false, 2, false)}"]
+              "Size: #{::Storage::byte_to_humanstring(1024 * @partition.size_k, false, 2, false)}" ]
+
+      tmp << "Device Path: #{@partition.udev_path}"
+
+      @partition.udev_ids.each_with_index do |udev_id, i|
+        tmp << "Device ID #{i + 1}: #{udev_id}"
+      end
 
       contents = Yast::HTML.List(tmp)
 
