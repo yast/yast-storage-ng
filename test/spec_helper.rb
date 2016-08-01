28447d7c6d0db31e45e6b6af2adb1c4288725095
Ancor Gonzalez Sosa <ancor@suse.de>
2016-08-01 16:23:40 +0200

    Fix test coverage calculation

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -22,13 +22,6 @@ DATA_PATH = File.expand_path("../data", __FILE__)
 ENV["Y2DIR"] = SRC_PATH
 
 require "yast"
-require_relative "support/storage_matchers"
-require_relative "support/storage_helpers"
-
-RSpec.configure do |c|
-  c.include Yast::RSpec::StorageMatchers
-  c.include Yast::RSpec::StorageHelpers
-end
 
 if ENV["COVERAGE"]
   require "simplecov"
@@ -47,4 +40,12 @@ if ENV["COVERAGE"]
       Coveralls::SimpleCov::Formatter
     ]
   end
+end
+
+require_relative "support/storage_matchers"
+require_relative "support/storage_helpers"
+
+RSpec.configure do |c|
+  c.include Yast::RSpec::StorageMatchers
+  c.include Yast::RSpec::StorageHelpers
 end
