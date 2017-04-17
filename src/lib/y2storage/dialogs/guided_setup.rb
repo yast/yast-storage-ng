b603dade349788b3db6a9a0737129796b15a6292
José Iván López González <jlopez@suse.com>
2017-04-17 13:20:27 +0100

    Add logic to skip root selection dialog

--- src/lib/y2storage/dialogs/guided_setup.rb
+++ src/lib/y2storage/dialogs/guided_setup.rb
@@ -41,36 +41,61 @@ module Y2Storage
         @settings = settings.dup
       end
 
-      # Disk analyzer to recover disks info
+      # Disk analyzer to recover disks info.
       def analyzer
         StorageManager.instance.probed_disk_analyzer
       end
 
-      # Executes steps of the wizard. Updates settings with user selections.
+      # Executes steps of the wizard.
       # @return [Symbol] last step result.
       def run
-        aliases = {
-          "select_disks"      => -> { SelectDisks.new(self).run },
-          "select_root_disk"  => -> { SelectRootDisk.new(self).run },
-          "select_scheme"     => -> { SelectScheme.new(self).run },
-          "select_filesystem" => -> { SelectFilesystem.new(self).run }
-        }
+        skip_needless_steps
+        Yast::Sequencer.Run(aliases, sequence)
+      end
 
-        sequence = {
-          "ws_start"          => "select_disks",
+    private
+
+      def sequence
+        {
+          "ws_start"          => @ws_start || "select_disks",
           "select_disks"      => { next: "select_root_disk", back: :back, abort: :abort },
           "select_root_disk"  => { next: "select_scheme", back: :back, abort: :abort },
           "select_scheme"     => { next: "select_filesystem", back: :back,  abort: :abort },
           "select_filesystem" => { next: :next, back: :back,  abort: :abort }
         }
+      end
 
-        # Skip disks selection when there is only one candidate
-        if analyzer.candidate_disks.size == 1
-          settings.candidate_devices = analyzer.candidate_disks.map(&:name)
-          sequence["ws_start"] = "select_root_disk"
-        end
+      def aliases
+        {
+          "select_disks"      => -> { SelectDisks.new(self).run },
+          "select_root_disk"  => -> { SelectRootDisk.new(self).run },
+          "select_scheme"     => -> { SelectScheme.new(self).run },
+          "select_filesystem" => -> { SelectFilesystem.new(self).run }
+        }
+      end
 
-        Yast::Sequencer.Run(aliases, sequence)
+      def skip_needless_steps
+        skip_select_disks if skip_select_disks?
+        skip_select_root_disk if skip_select_root_disk?
+      end
+
+      def skip_select_disks?
+        analyzer.candidate_disks.size == 1
+      end
+
+      def skip_select_disks
+        settings.candidate_devices = analyzer.candidate_disks.map(&:name)
+        @ws_start = "select_root_disk"
+      end
+
+      def skip_select_root_disk?
+        skip_select_disks? &&
+          analyzer.installed_systems(analyzer.candidate_disks.first).size == 0
+      end
+
+      def skip_select_root_disk
+        settings.root_device = analyzer.candidate_disks.first
+        @ws_start = "select_scheme"
       end
     end
   end
