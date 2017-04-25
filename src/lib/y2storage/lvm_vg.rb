8788dc3de011e397bd7d65b9d8852550c7f373a9
Ancor Gonzalez Sosa <ancor@suse.de>
2017-04-25 10:29:42 +0200

    Documentation for the LVM classes

--- src/lib/y2storage/lvm_vg.rb
+++ src/lib/y2storage/lvm_vg.rb
@@ -29,28 +29,107 @@ module Y2Storage
   class LvmVg < Device
     wrap_class Storage::LvmVg
 
-    # Volume group name. This is different from the device name
+    # @!attribute vg_name
+    #   @return [String] volume group name (e.g."vg0"), not to be confused
+    #     with BlkDevice#name (e.g. "/dev/mapper/vg0")
     storage_forward :vg_name
     storage_forward :vg_name=
 
+    # @!method size
+    #   @return [DiskSize]
     storage_forward :size, as: "DiskSize"
-    storage_forward :extent_size, as: "DiskSize"
-    storage_forward :extent_size=
 
+    # @!attribute extent_size
+    #   Size of one extent
+    #
+    #   Each logical volume is split into chunks of data, known as logical
+    #   extents. The extent size is the same for all logical volumes in the
+    #   volume group.
+    #
+    #   Setting the extent_size can modify the size of the logical volumes.
+    #
+    #   @raise Storage::InvalidExtentSize
+    #
+    #   @return [DiskSize]
+    storage_forward :extent_size, as: "DiskSize"
+    storage_forward :extent_size=, as: "DiskSize"
+
+    # @!method number_of_extents
+    #   Total number of extents
+    #   @see #extent_size
+    #
+    #   @return [Fixnum] total number of extents
     storage_forward :number_of_extents
+
+    # @!method number_of_used_extents
+    #   Total number of extents currently in use. May be larger than the number
+    #   of extents in the volume group.
+    #   @see #extent_size
+    #
+    #   @return [Fixnum]
     storage_forward :number_of_used_extents
+
+    # @!method number_of_free_extents
+    #   Total number of available extents
+    #   @see #extent_size
+    #
+    #   @return [Fixnum]
     storage_forward :number_of_free_extents
 
+    # @!method lvm_pvs
+    #   @return [Array<LvmPv>] physical volumes in the VG
     storage_forward :lvm_pvs, as: "LvmPv"
+
+    # @!method add_lvm_pv(blk_device)
+    #   Adds a block device as a physical volume to the volume group. If there
+    #   is not a physical volume on the block device it will be created.
+    #
+    #   @param blk_device [BlkDevice]
+    #   @return [LvmPv]
     storage_forward :add_lvm_pv, as: "LvmPv"
+
+    # @!method remove_lvm_pv(blk_device)
+    #   Removes a block device from the volume group. The physical volume on the
+    #   block device will the deleted.
+    #
+    #   @param blk_device [BlkDevice]
     storage_forward :remove_lvm_pv
 
+    # @!method lvm_lvs
+    #   @return [Array<LvmLv>] logical volumes in the VG
     storage_forward :lvm_lvs, as: "LvmLv"
+
+    # @!method create_lvm_lv(lv_name, size)
+    #   Creates a logical volume in the volume group.
+    #
+    #   @param lv_name [String] name of the new volume. @see LvmLv#lv_name
+    #   @param size [DiskSize] size of the new volume
+    #   @return [LvmLv]
     storage_forward :create_lvm_lv, as: "LvmLv"
+
+    # @!method delete_lvm_lv(lvm_lv)
+    #   Deletes a logical volume in the volume group. Also deletes all
+    #   descendants of the logical volume.
+    #
+    #   @param lvm_lv [LvmLv] volume to delete
     storage_forward :delete_lvm_lv
 
+    # @!method self.create(devicegraph, vg_name)
+    #   @param devicegraph [Devicegraph]
+    #   @param vg_name [String] @see #vg_name
+    #   return [LvmVg]
     storage_class_forward :create, as: "LvmVg"
+
+    # @!method self.all(devicegraph)
+    #   @param devicegraph [Devicegraph]
+    #   @return [Array<LvmVg>] all the volume groups in the given devicegraph,
+    #     sorted by #vg_name
     storage_class_forward :all, as: "LvmVg"
+
+    # @!method self.find_by_vg_name(devicegraph, vg_name)
+    #   @param devicegraph [Devicegraph]
+    #   @param vg_name [String] name of the volume group. @see #vg_name
+    #   @return [LvmVg] nil if there is no such volume group
     storage_class_forward :find_by_vg_name, as: "LvmVg"
 
   protected
