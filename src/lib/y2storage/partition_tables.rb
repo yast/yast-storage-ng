00476c8ee002889d62f529b0bbbb1e55cf91a4f3
Arvin Schnell <aschnell@suse.de>
2017-06-06 14:40:28 +0200

    - added wrapper for DASD partition table

--- src/lib/y2storage/partition_tables.rb
+++ src/lib/y2storage/partition_tables.rb
@@ -1,4 +1,5 @@
 require "y2storage/partition_tables/type"
 require "y2storage/partition_tables/msdos"
 require "y2storage/partition_tables/gpt"
+require "y2storage/partition_tables/dasd"
 require "y2storage/partition_tables/partition_slot"
