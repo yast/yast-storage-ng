2d710ddb3176f3e1560c8e3e26977a4fbfba9540
José Iván López González <jlopez@suse.com>
2017-06-09 13:21:26 +0100

    Add client partitions_proposal

---
+++ src/lib/y2storage/clients/partitions_proposal.rb
@@ -0,0 +1,117 @@
+#!/usr/bin/env ruby
+#
+# encoding: utf-8
+
+# Copyright (c) [2017] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "yast"
+require "y2storage"
+require "y2storage/dialogs/actions_presenter"
+require "installation/proposal_client"
+
+Yast.import "Popup"
+
+module Y2Storage
+  module Clients
+    # Proposal client to show the list of storage actions
+    class PartitionsProposal < ::Installation::ProposalClient
+      include Yast::Logger
+
+      def self.state(storage_manager)
+        return if staging_revision == storage_manager.staging_revision
+
+        self.staging_revision = storage_manager.staging_revision
+
+        staging = storage_manager.y2storage_staging
+        actiongraph = staging ? staging.actiongraph : nil
+        self.actions_presenter = ActionsPresenter.new(actiongraph)
+      end
+
+      def initialize
+        create_proposal
+        self.class.state(storage_manager)
+      end
+
+      def make_proposal(_attrs)
+        {
+          "preformatted_proposal" => actions_presenter.to_html,
+          "links"                 => actions_presenter.events,
+          "language_changed"      => false
+        }
+      end
+
+      def ask_user(param)
+        event = param["chosen_id"]
+
+        if actions_presenter.events.include?(event)
+          actions_presenter.update_status(event)
+          result = :again
+        else
+          Yast::Report.Warning(_("This action is not enabled at this moment"))
+          result = :back
+        end
+
+        { "workflow_sequence" => result }
+      end
+
+      def description
+        {
+          "id"              => "partitions",
+          "rich_text_title" => _("Partitioning"),
+          "menu_title"      => _("&Partitioning")
+        }
+      end
+
+    private
+
+      class << self
+        attr_accessor :staging_revision
+        attr_accessor :actions_presenter
+      end
+
+      def actions_presenter
+        self.class.actions_presenter
+      end
+
+      def staging_revision
+        self.class.staging_revision
+      end
+
+      def storage_manager
+        StorageManager.instance
+      end
+
+      def create_proposal
+        proposal = storage_manager.proposal || new_proposal
+        proposal.propose if !proposal.proposed?
+        storage_manager.proposal = proposal if storage_manager.proposal.nil?
+      rescue Y2Storage::Proposal::Error
+        log.error("generating proposal failed")
+      end
+
+      def new_proposal
+        settings = ProposalSettings.new_for_current_product
+        probed = storage_manager.y2storage_probed
+        disk_analyzer = storage_manager.probed_disk_analyzer
+        Proposal.new(settings: settings, devicegraph: probed, disk_analyzer: disk_analyzer)
+      end
+    end
+  end
+end
---
+++ test/clients/partitions_proposal_test.rb
@@ -0,0 +1,182 @@
+#!/usr/bin/env rspec
+# encoding: utf-8
+
+# Copyright (c) [2017] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "../spec_helper"
+require "y2storage/clients/partitions_proposal"
+
+describe Y2Storage::Clients::PartitionsProposal do
+  subject { described_class.new }
+
+  before do
+    Y2Storage::StorageManager.fake_from_yaml(input_file_for("empty_hard_disk_gpt_50GiB"))
+    # To generate a new PartitionsProposal.actions_presenter for each test
+    described_class.staging_revision = 0
+  end
+
+  let(:actions_presenter) { described_class.actions_presenter }
+
+  describe "#initialize" do
+    let(:storage_manager) { Y2Storage::StorageManager.instance }
+
+    context "when there is not a proposal" do
+      it "calculates a new proposal" do
+        expect(storage_manager.proposal).to be_nil
+        subject
+        expect(storage_manager.proposal).not_to be_nil
+        expect(storage_manager.proposal.proposed?).to be true
+      end
+    end
+
+    context "when a proposal already exists" do
+      before do
+        allow(storage_manager).to receive(:proposal).and_return(proposal)
+      end
+
+      let(:proposal) { instance_double(Y2Storage::Proposal, proposed?: proposed) }
+
+      context "but it is not calculated yet" do
+        let(:proposed) { false }
+
+        it "calculates the proposal" do
+          expect(storage_manager.proposal).to receive(:propose)
+          subject
+        end
+      end
+
+      context "and it is already calculated" do
+        let(:proposed) { true }
+
+        it "does not re-calculate the proposal" do
+          expect(storage_manager.proposal).to_not receive(:propose)
+          subject
+        end
+      end
+    end
+
+    context "when it has the current staging revision" do
+      before do
+        described_class.staging_revision = 1
+        described_class.actions_presenter = presenter
+      end
+
+      let(:presenter) { instance_double(Y2Storage::ActionsPresenter) }
+
+      it "does not update the staging revision" do
+        subject
+        expect(described_class.staging_revision).to eq(1)
+      end
+
+      it "does not update the actions presenter" do
+        subject
+        expect(described_class.actions_presenter).to eq(presenter)
+      end
+    end
+
+    context "when it has not the current staging revision" do
+      before do
+        described_class.staging_revision = 1
+        described_class.actions_presenter = presenter1
+        allow(storage_manager).to receive(:staging_revision).and_return(2)
+        allow(Y2Storage::ActionsPresenter).to receive(:new).and_return(presenter2)
+      end
+
+      let(:presenter1) { instance_double(Y2Storage::ActionsPresenter) }
+      let(:presenter2) { instance_double(Y2Storage::ActionsPresenter) }
+
+      it "updates the staging revision" do
+        subject
+        expect(described_class.staging_revision).to eq 2
+      end
+
+      it "updates the actions presenter" do
+        subject
+        expect(described_class.actions_presenter).to eq(presenter2)
+      end
+    end
+  end
+
+  describe "#make_proposal" do
+    it "returns a hash with 'preformatted_proposal', 'links' and 'language_changed'" do
+      proposal = subject.make_proposal({})
+      expect(proposal).to be_a Hash
+      expect(proposal).to include("preformatted_proposal", "links", "language_changed")
+    end
+
+    it "returns html representation for 'preformatted_proposal' key" do
+      proposal = subject.make_proposal({})
+      html_content = actions_presenter.to_html
+      expect(proposal["preformatted_proposal"]).to eq html_content
+    end
+
+    it "includes actions presenter events in 'links' value" do
+      proposal = subject.make_proposal({})
+      presenter_events = actions_presenter.events
+      expect(proposal["links"]).to include(*presenter_events)
+    end
+
+    it "returns false for 'language_changed' key" do
+      proposal = subject.make_proposal({})
+      expect(proposal["language_changed"]).to be false
+    end
+  end
+
+  describe "#ask_user" do
+    context "when 'chosen_id' is an actions presenter event" do
+      let(:param) { { "chosen_id" => actions_presenter.events.first } }
+
+      it "changes status of actions presenter" do
+        subject
+        expect(actions_presenter).to receive(:update_status)
+        subject.ask_user(param)
+      end
+
+      it "returns a hash with :again for 'workflow_sequence' key" do
+        result = subject.ask_user(param)
+        expect(result).to be_a(Hash)
+        expect(result["workflow_sequence"]).to eq :again
+      end
+    end
+
+    context "when 'chosen_id' is not an actions presenter event" do
+      let(:param) { { "chosen_id" => "whatever" } }
+
+      it "shows a warning dialog" do
+        expect(Yast::Report).to receive(:Warning)
+        subject.ask_user(param)
+      end
+
+      it "returns a hash with :back for 'workflow_sequence' key" do
+        result = subject.ask_user(param)
+        expect(result).to be_a(Hash)
+        expect(result["workflow_sequence"]).to eq :back
+      end
+    end
+  end
+
+  describe "#description" do
+    it "returns a hash with 'id', 'rich_text_title' and 'menu_title'" do
+      description = subject.description
+      expect(description).to be_a Hash
+      expect(description).to include("id", "menu_title", "rich_text_title")
+    end
+  end
+end
