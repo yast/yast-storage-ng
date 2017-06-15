889821ba36c0270e62c63916e5b07e747cc4cac8
Josef Reidinger <jreidinger@suse.cz>
2017-06-15 09:24:43 +0200

    make rubocop happy

--- test/storage_manager_test.rb
+++ test/storage_manager_test.rb
@@ -86,14 +86,14 @@ describe Y2Storage::StorageManager do
     end
 
     it "initializes #storage with the mocked devicegraphs" do
-      result = described_class.fake_from_xml(input_file_for("md2-devicegraph", suffix: "xml"))
+      described_class.fake_from_xml(input_file_for("md2-devicegraph", suffix: "xml"))
       expect(manager.storage).to be_a Storage::Storage
       expect(Storage::Disk.all(manager.probed).size).to eq 4
       expect(Storage::Disk.all(manager.staging).size).to eq 4
     end
 
     it "initializes #staging_revision" do
-      result = described_class.fake_from_xml(input_file_for("md2-devicegraph", suffix: "xml"))
+      described_class.fake_from_xml(input_file_for("md2-devicegraph", suffix: "xml"))
       expect(manager.staging_revision).to be_zero
     end
   end
