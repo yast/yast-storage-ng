393561ef966d9b40c855e8c7f7cb8860631681e7
Josef Reidinger <jreidinger@suse.cz>
2017-06-23 16:36:34 +0200

    add comment

--- test/fake_dependencies/lib/y2partitioner/clients/main.rb
+++ test/fake_dependencies/lib/y2partitioner/clients/main.rb
@@ -1,5 +1,7 @@
 module Y2Partitioner
   module Clients
+    # Just fake client from partitioner. It is entry point to partitioner and currently only API
+    # that is used from partitioner in storage.
     class Main
       def self.run
       end
