3578fb34a5618b8cd6ee44a1304bbba07fccac7d
José Iván López González <jlopez@suse.com>
2017-07-21 18:13:51 +0100

    Add comment to skipped test

--- test/y2storage/region_test.rb
+++ test/y2storage/region_test.rb
@@ -75,6 +75,7 @@ describe Y2Storage::Region do
     context "if both regions have a different block size" do
       let(:other) { Y2Storage::Region.create(100, 1000, 2.KiB) }
 
+      # Pending on exception binding in libstorage-ng.
       xit "raises Storage::DifferentBlockSizes" do
         expect { region.inside?(other) }.to raise_error Storage::DifferentBlockSizes
       end
