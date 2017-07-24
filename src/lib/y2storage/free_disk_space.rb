ede7ddbbb4f57dbd4b8e8f0756098a1facd2c328
José Iván López González <jlopez@suse.com>
2017-07-24 08:33:46 +0100

    Remove comment

--- src/lib/y2storage/free_disk_space.rb
+++ src/lib/y2storage/free_disk_space.rb
@@ -46,7 +46,6 @@ module Y2Storage
       @disk = disk
       # Store a duplicate of the original region, which could change or be
       # deleted (don't trust the garbage collector when SWIG is involved)
-      # TODO: #to_storage_value to be removed when adapting this class
       region = Storage::Region.new(region.to_storage_value)
       @region = Y2Storage::Region.new(region)
     end
