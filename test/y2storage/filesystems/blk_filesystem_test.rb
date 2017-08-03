de84bd5861b66734fd849849473660fb4c76d4aa
José Iván López González <jlopez@suse.com>
2017-08-03 16:00:27 +0100

    Add tests for new BlkFilesystem methods

--- test/y2storage/filesystems/blk_filesystem_test.rb
+++ test/y2storage/filesystems/blk_filesystem_test.rb
@@ -28,7 +28,7 @@ describe Y2Storage::Filesystems::BlkFilesystem do
   before do
     fake_scenario(scenario)
   end
-  let(:scenario) { "mixed_disks" }
+  let(:scenario) { "mixed_disks_btrfs" }
   let(:blk_device) { Y2Storage::BlkDevice.find_by_name(fake_devicegraph, dev_name) }
   subject(:filesystem) { blk_device.blk_filesystem }
 
@@ -46,6 +46,25 @@ describe Y2Storage::Filesystems::BlkFilesystem do
 
       it "returns false" do
         expect(filesystem.supports_btrfs_subvolumes?).to eq false
+      end
+    end
+  end
+
+  describe "#btrfs_subvolumes" do
+    context "for a no-Btrfs filesystem" do
+      let(:dev_name) { "/dev/sdb3" }
+
+      it "returns an empty array" do
+        expect(filesystem.btrfs_subvolumes).to eq []
+      end
+    end
+
+    context "for a Btrfs filesystem" do
+      let(:dev_name) { "/dev/sdb2" }
+
+      it "returns an array of BtrfsSubvolume objects" do
+        expect(filesystem.btrfs_subvolumes).to be_a Array
+        expect(filesystem.btrfs_subvolumes).to all(be_a(Y2Storage::BtrfsSubvolume))
       end
     end
   end
@@ -70,21 +89,119 @@ describe Y2Storage::Filesystems::BlkFilesystem do
     end
   end
 
-  describe "#btrfs_subvolumes" do
+  describe "#default_btrfs_subvolume" do
     context "for a no-Btrfs filesystem" do
-      let(:dev_name) { "/dev/sdb3" }
+      let(:dev_name) { "/dev/sda1" }
 
-      it "returns an empty array" do
-        expect(filesystem.btrfs_subvolumes).to eq []
+      it "returns nil" do
+        expect(filesystem.default_btrfs_subvolume).to eq nil
       end
     end
 
     context "for a Btrfs filesystem" do
-      let(:dev_name) { "/dev/sdb2" }
+      let(:dev_name) { "/dev/sda2" }
 
-      it "returns an array of BtrfsSubvolume objects" do
-        expect(filesystem.btrfs_subvolumes).to be_a Array
-        expect(filesystem.btrfs_subvolumes).to all(be_a(Y2Storage::BtrfsSubvolume))
+      it "returns a BtrfsSubvolume object" do
+        expect(filesystem.default_btrfs_subvolume).to be_a(Y2Storage::BtrfsSubvolume)
+      end
+
+      it "returns the default subvolume" do
+        expect(filesystem.default_btrfs_subvolume.path).to eq("@")
+      end
+    end
+  end
+
+  describe "#default_btrfs_subvolume_path" do
+    context "for a no-Btrfs filesystem" do
+      let(:dev_name) { "/dev/sda1" }
+
+      it "returns nil" do
+        expect(filesystem.default_btrfs_subvolume_path).to eq nil
+      end
+    end
+
+    context "for a Btrfs filesystem" do
+      let(:dev_name) { "/dev/sda2" }
+
+      it "returns the default path for a default btrfs subvolume" do
+        expect(filesystem.default_btrfs_subvolume_path).to eq("@")
+      end
+    end
+  end
+
+  describe "#find_btrfs_subvolume_by_path" do
+    context "for a no-Btrfs filesystem" do
+      let(:dev_name) { "/dev/sda1" }
+
+      it "returns nil" do
+        expect(filesystem.find_btrfs_subvolume_by_path("")).to eq nil
+      end
+    end
+
+    context "for a Btrfs filesystem" do
+      let(:dev_name) { "/dev/sda2" }
+
+      context "where exists a subvolume with the searched path" do
+        it "returns the subvolume with that path" do
+          path = "@/home"
+          subvolume = filesystem.find_btrfs_subvolume_by_path(path)
+
+          expect(subvolume).to be_a(Y2Storage::BtrfsSubvolume)
+          expect(subvolume.path).to eq(path)
+        end
+      end
+
+      context "where does not exist a subvolume with the searched path" do
+        # TODO: Needed bindings for Storage::BtrfsSubvolumeNotFoundByPath
+        xit "returns nil" do
+          expect(filesystem.find_btrfs_subvolume_by_path("@/foo")).to be_nil
+        end
+      end
+    end
+  end
+
+  describe "#ensure_default_btrfs_subvolume" do
+    context "for a no-Btrfs filesystem" do
+      let(:dev_name) { "/dev/sda1" }
+
+      it "returns nil" do
+        expect(filesystem.ensure_default_btrfs_subvolume).to eq nil
+      end
+    end
+
+    context "for a Btrfs filesystem" do
+      context "where the default subvolume is equal to the top level one" do
+        let(:dev_name) { "/dev/sdb2" }
+
+        it "creates a default subvolume with the correct path" do
+          top_level = filesystem.top_level_btrfs_subvolume
+          default = filesystem.default_btrfs_subvolume
+
+          expect(default).to eq(top_level)
+          expect(default.path).to eq("")
+
+          default = filesystem.ensure_default_btrfs_subvolume
+
+          expect(filesystem.default_btrfs_subvolume).to eq(default)
+          expect(default.path).to eq(filesystem.default_btrfs_subvolume_path)
+        end
+      end
+
+      context "where the default subvolume is different to the top level one" do
+        let(:dev_name) { "/dev/sda2" }
+
+        it "does not create a new subvolume" do
+          subvolumes = filesystem.btrfs_subvolumes
+          filesystem.ensure_default_btrfs_subvolume
+
+          expect(filesystem.btrfs_subvolumes).to eq(subvolumes)
+        end
+
+        it "returns the current default subvolume" do
+          default = filesystem.default_btrfs_subvolume
+
+          expect(filesystem.ensure_default_btrfs_subvolume).to eq(default)
+        end
       end
     end
   end
