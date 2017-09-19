67f0a8959a9759cde20585b4633a777947aefdee
José Iván López González <jlopez@suse.com>
2017-09-19 17:08:51 +0100

    Fix tests

--- test/y2partitioner/dialogs/format_and_mount_test.rb
+++ test/y2partitioner/dialogs/format_and_mount_test.rb
@@ -4,7 +4,7 @@ require "cwm/rspec"
 require "y2partitioner/dialogs/format_and_mount"
 
 describe Y2Partitioner::Dialogs::FormatAndMount do
-  let(:controller) { double("FilesystemController", blk_device: partition) }
+  let(:controller) { double("FilesystemController", blk_device: partition, filesystem: nil) }
   let(:partition) { double("Partition", name: "/dev/sda1") }
 
   subject { described_class.new(controller) }
