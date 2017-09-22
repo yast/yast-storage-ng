8fb5f4ab6ccf742832fceca234a8517af98008ec
Ancor Gonzalez Sosa <ancor@suse.de>
2017-09-22 06:55:11 +0200

    Please coveralls

--- test/y2partitioner/dialogs/format_and_mount_test.rb
+++ test/y2partitioner/dialogs/format_and_mount_test.rb
@@ -25,17 +25,32 @@ describe Y2Partitioner::Dialogs::FormatAndMount::FormatMountOptions do
     before do
       allow(Y2Partitioner::Widgets::FormatOptions).to receive(:new).and_return format_widget
       allow(Y2Partitioner::Widgets::MountOptions).to receive(:new).and_return mount_widget
+      allow(format_widget).to receive(:refresh)
       allow(mount_widget).to receive(:refresh)
     end
 
-    it "does not call #refresh for the widget triggering the update" do
-      expect(format_widget).to_not receive(:refresh)
-      widget.refresh_others(format_widget)
+    context "when the FormatOptions widget triggers an update" do
+      it "does not call #refresh for the widget triggering the update" do
+        expect(format_widget).to_not receive(:refresh)
+        widget.refresh_others(format_widget)
+      end
+
+      it "calls #refresh for the widget not triggering the update" do
+        expect(mount_widget).to receive(:refresh)
+        widget.refresh_others(format_widget)
+      end
     end
 
-    it "calls #refresh for the widget not triggering the update" do
-      expect(mount_widget).to receive(:refresh)
-      widget.refresh_others(format_widget)
+    context "when the MountOptions widget triggers an update" do
+      it "does not call #refresh for the widget triggering the update" do
+        expect(mount_widget).to_not receive(:refresh)
+        widget.refresh_others(mount_widget)
+      end
+
+      it "calls #refresh for the widget not triggering the update" do
+        expect(format_widget).to receive(:refresh)
+        widget.refresh_others(mount_widget)
+      end
     end
   end
 end
