a5b7170532616b8462fddfc420b8479422eb10cd
José Iván López González <jlopez@suse.com>
2017-10-18 17:55:11 +0100

    Add BlkDeviceButton base class

--- src/lib/y2partitioner/widgets/edit_blk_device_button.rb
+++ src/lib/y2partitioner/widgets/edit_blk_device_button.rb
@@ -1,6 +1,7 @@
 require "yast"
 require "cwm"
 require "y2partitioner/sequences/edit_blk_device"
+require "y2partitioner/widgets/blk_device_button"
 require "y2partitioner/ui_state"
 
 Yast.import "Popup"
@@ -9,45 +10,16 @@ module Y2Partitioner
   module Widgets
     # Button for opening the editing workflow (basically mount and format
     # options) on a block device.
-    class EditBlkDeviceButton < CWM::PushButton
-      # Constructor
-      # @param device [Y2Storage::BlkDevice]
-      # @param table [Y2Partitioner::Widgets::ConfigurableBlkDevicesTable]
-      def initialize(device: nil, table: nil)
-        textdomain "storage"
-
-        unless device || table
-          raise ArgumentError, "Please provide either a block device or a table with devices"
-        end
-
-        @device = device
-        @table = table
-      end
-
+    class EditBlkDeviceButton < BlkDeviceButton
       def label
         _("Edit...")
       end
 
-      # @macro seeAbstractWidget
-      def handle
-        if device.nil?
-          Yast::Popup.Error(_("No device selected"))
-          return nil
-        end
-
+      # @see BlkDeviceButton#actions
+      def actions
         UIState.instance.select_row(device.sid)
         Sequences::EditBlkDevice.new(device).run
-
-        # sym == :next ? :redraw : nil
-        # must redraw because we've replaced the original dialog contents!
         :redraw
-      end
-
-    protected
-
-      # Device to edit
-      def device
-        @device || @table.selected_device
       end
     end
   end
