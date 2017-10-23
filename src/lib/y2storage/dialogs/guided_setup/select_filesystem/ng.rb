4a4098d107db3f71abb2e1c38124582e4bfbd7e6
Stefan Hundhammer <shundhammer@suse.de>
2017-10-23 18:44:05 +0200

    Handle arbitrary volumes

--- src/lib/y2storage/dialogs/guided_setup/select_filesystem/ng.rb
+++ src/lib/y2storage/dialogs/guided_setup/select_filesystem/ng.rb
@@ -27,85 +27,319 @@ require "y2storage/filesystems/type"
 module Y2Storage
   module Dialogs
     class GuidedSetup
+      # Dialog to select filesystems.
       class SelectFilesystem
-        # Dialog to select filesystems.
         # This is the more advanced version for the NG-style proposal settings
         # that support more than just a separate home volume.
         # See also SelectFilesystem::Legacy.
+        #
         class Ng < Base
-          def root_filesystem_handler
-            filesystem = Filesystems::Type.find(widget_value(:root_filesystem))
-            widget_update(:snapshots, filesystem.is?(:btrfs), attr: :Enabled)
-          end
+          attr_reader :root_vol, :home_vol, :swap_vol, :other_volumes
 
-          def separate_home_handler
-            widget_update(:home_filesystem, widget_value(:separate_home), attr: :Enabled)
+          def initialize(*params)
+            super
+            @root_vol = find_vol("/")
+            @home_vol = find_vol("/home")
+            @swap_vol = find_vol("swap")
+            @other_volumes = find_other_volumes
           end
 
         protected
 
+          # Enable or disable widgets for the root volume
+          #
+          def root_vol_handler
+            fs_type = widget_value(:root_fs_type)
+            return if fs_type.nil?
+            fs_type = Filesystems::Type.find(fs_type)
+
+            enable_snapshots = fs_type.is?(:btrfs)
+            enable_snapshots &&= @root_vol.snapshots_configurable? unless @root_vol.nil?
+            widget_update(:snapshots, enable_snapshots, attr: :Enabled)
+          end
+
+          # Enable or disable widgets for the home volume
+          #
+          def home_vol_handler
+            vol_handler(@home_vol)
+          end
+
+          # Enable or disable widgets for the swap volume
+          #
+          def swap_vol_handler
+            return if @swap_vol.nil?
+            widget_update(:enlarge_swap, @swap_vol.adjust_by_ram_configurable?, attr: :Enabled)
+          end
+
+          # Enable or disable widgets for all 'other' volumes
+          #
+          def other_volumes_handler
+            @other_volumes.each { |vol| vol_handler(vol) }
+          end
+
+          # Enable or disable widgets for one volume that has a "propose" check
+          # box and possibly a filesystem selection combo box (i.e. home and
+          # any of the 'other' volumes)
+          #
+          # @param vol [VolumeSpecification]
+          #
+          def vol_handler(vol)
+            return if vol.nil?
+            return unless fs_type_user_configurable?(vol)
+            vol_name = vol.mount_point
+            propose = widget_value(propose_widget_id(vol_name))
+            combo_box_id = fs_type_widget_id(vol_name)
+            widget_update(combo_box_id, propose, attr: :Enabled)
+            # Make sure something is selected
+            current_val = widget_value(combo_box_id)
+            widget_update(combo_box_id, vol.fs_type.to_sym) if current_val.nil?
+          end
+
+          # Return a widget term for the dialog content, i.e. all the volumes
+          # and possibly some more interactive widgets.
+          #
+          # @return [WidgetTerm]
+          #
           def dialog_content
-            HSquash(
-              VBox(
-                root_filesystem_widget,
-                VSpacing(2),
-                home_filesystem_widget
-              )
+            fs = [root_vol_widget, home_vol_widget]
+            fs << other_volumes_widgets
+            fs << swap_vol_widget
+            fs.flatten!.compact!
+            HBox(
+              fs.each_with_object(VBox()) do |vbox, widget|
+                vbox << VSpacing(2) unless vbox.empty?
+                vbox << widget
+              end
             )
           end
 
-          def root_filesystem_widget
+          # Return a widget term for the root volume.
+          #
+          # @return [WidgetTerm]
+          #
+          def root_vol_widget
+            return nil if @root_vol.nil?
+            fs_types = @root_vol.fs_types || Filesystems::Type.root_filesystems
+            items = fs_types.map do |fs|
+              Item(Id(fs.to_sym), fs.to_human_string, fs == @root_vol.fs_type)
+            end
             VBox(
               Left(
                 ComboBox(
-                  Id(:root_filesystem), Opt(:notify), _("File System for Root Partition"),
-                  Filesystems::Type.root_filesystems.map { |f| Item(Id(f.to_sym), f.to_human_string) }
+                  Id(:root_fs_type), Opt(:notify), _("File System for Root Partition"), items
                 )
               ),
               Left(
                 HBox(
                   HSpacing(3),
-                  Left(CheckBox(Id(:snapshots), _("Enable Snapshots"), true))
+                  Left(CheckBox(Id(:snapshots), _("Enable Snapshots"), @root_vol.snapshots?))
                 )
               )
             )
           end
 
-          def home_filesystem_widget
+          # Return a widget term for the home volume.
+          #
+          # @return [WidgetTerm]
+          #
+          def home_vol_widget
+            # Translators: name of the partition that holds the users' home directories (/home)
+            vol_widget(@home_vol, _("Home"))
+          end
+
+          # Return a widget term for the swap volume.
+          #
+          # @return [WidgetTerm]
+          #
+          def swap_vol_widget
+            return nil if @swap_vol.nil?
+            Left(
+              CheckBox(
+                Id(:enlarge_swap),
+                _("Enlarge Swap for Suspend"),
+                @swap_vol.adjust_by_ram?
+              )
+            )
+          end
+
+          # Return the widgets for the 'other' volumes, i.e. all except root, home, swap.
+          #
+          # @return [Array<WidgetTerm>]
+          #
+          def other_volumes_widgets
+            @other_volumes.each_with_object([]) { |vol| vol_widget(vol) }
+          end
+
+          # Return a widget term for a volume with an optional name.
+          # If not specified, use the mount point as the name.
+          #
+          # @param vol [VolumeSpecification]
+          # @param vol_name [String]
+          # @return [WidgetTerm]
+          def vol_widget(vol, vol_name = nil)
+            return nil if vol.nil?
+            return nil unless vol.proposed? || vol.proposed_configurable?
+            vol_name ||= vol.mount_point
             VBox(
-              Left(
-                CheckBox(Id(:separate_home), Opt(:notify), _("Propose Separate Home Partition"))
-              ),
-              Left(
-                HBox(
-                  HSpacing(2),
-                  ComboBox(
-                    Id(:home_filesystem), _("File System for Home Partition"),
-                    Filesystems::Type.home_filesystems.map do |filesystem|
-                      Item(Id(filesystem.to_sym), filesystem.to_human_string)
-                    end
-                  )
+              propose_widget(vol, vol_name),
+              fs_type_widget(vol, vol_name)
+            )
+          end
+
+          # Return a widget term for the checkbox to select if a partition should be proposed.
+          # The checkbox might be disabled if the user is not allowed to change this value.
+          # The idea is to show the user even in that case that the partition
+          # will be created, even if he cannot prevent that.
+          #
+          # @param vol [VolumeSpecification]
+          # @param vol_name [String]
+          # @return [WidgetTerm]
+          #
+          def propose_widget(vol, vol_name)
+            cb_opt = Opt(:notify)
+            cb_opt << :disabled unless vol.proposed_configurable?
+            Left(
+              CheckBox(Id(propose_widget_id(vol_name)), cb_opt,
+                # Translators: %1 is the name of the partition ("root", "home", "/data", ...
+                Yast::Builtins.sformat(_("Propose Separate %1 Partition"), vol_name),
+                vol.proposed?)
+            )
+          end
+
+          # Return a widget term for a volume's filesystem type.
+          #
+          # @param vol [VolumeSpecification]
+          # @param vol_name [String]
+          # @return [WidgetTerm]
+          #
+          def fs_type_widget(vol, vol_name)
+            return Empty() unless fs_type_user_configurable?(vol)
+            fs_types = vol.fs_types || Filesystems::Type.home_filesystems
+            items = fs_types.map { |fs| Item(Id(fs.to_sym), fs.to_human_string) }
+            Left(
+              HBox(
+                HSpacing(2),
+                ComboBox(
+                  Id(fs_type_widget_id(vol_name)),
+                  # Translators: %1 is the name of the partition ("root", "home", "/data", ...
+                  Yast::Builtins.sformat(_("File System for %1 Partition"), vol_name),
+                  items
                 )
               )
             )
           end
 
-          def initialize_widgets
-            widget_update(:root_filesystem, settings.root_filesystem_type.to_sym)
-            widget_update(:snapshots, settings.use_snapshots)
-            widget_update(:home_filesystem, settings.home_filesystem_type.to_sym)
-            widget_update(:separate_home, settings.use_separate_home)
-            root_filesystem_handler
-            separate_home_handler
+          # Check if the filesystem type for a volume should be user configurable.
+          #
+          # @param vol [VolumeSpecification]
+          # @return [Boolean]
+          #
+          def fs_type_user_configurable?(vol)
+            return false if vol.fs_type == :swap
+            return false if vol.fs_types == [:swap]
+            true
           end
 
+          # Create a normalized ID for a volume: Use its name or mount point,
+          # strip any leading slash, replace any other special character with
+          # an underscore and simplify underscores.
+          # "/home" -> "home"
+          # "/var/lib/docker" -> "var_lib_docker"
+          # "any?! weird!! name__whatever" -> "any_weird_name_whatever"
+          #
+          # @param name [String]
+          # @return [String]
+          #
+          def normalized_id(name)
+            id = name.gsub(/[^a-zA-Z0-9_]/, "_")
+            id.gsub!(/__+/, "_")
+            id.gsub!(/^_/, "")
+            id.gsub!(/_$/, "")
+            id.downcase
+          end
+
+          # Return the widget ID of a volume's checkbox to enable or disable
+          # proposing it
+          #
+          # @param vol_name [String]
+          # @return [Symbol]
+          #
+          def propose_widget_id(vol_name)
+            ("propose_" + normalized_id(vol_name)).to_sym
+          end
+
+          # Return the widget ID of a volume's combo box to select the
+          # filesystem type
+          #
+          # @param vol_name [String]
+          # @return [Symbol]
+          #
+          def fs_type_widget_id(vol_name)
+            (normalized_id(vol_name) + "_fs_type").to_sym
+          end
+
+          # Initialize the interactive widgets
+          #
+          def initialize_widgets
+            widget_update(:root_fs_type, @root_vol.fs_type.to_sym) unless @root_vol.nil?
+            widget_update(:snapshots, @root_vol.snapshots?) unless @root_vol.nil?
+            widget_update(:enlarge_swap, @swap_vol.adjust_by_ram?) unless @swap_vol.nil?
+            root_vol_handler
+            home_vol_handler
+            swap_vol_handler
+            other_volumes_handler
+          end
+
+          # Update the settings: Fetch the current widget values and store them
+          # in the settings.
+          #
           def update_settings!
-            root_filesystem = Filesystems::Type.find(widget_value(:root_filesystem))
-            home_filesystem = Filesystems::Type.find(widget_value(:home_filesystem))
-            settings.root_filesystem_type = root_filesystem
-            settings.use_snapshots = widget_value(:snapshots)
-            settings.use_separate_home = widget_value(:separate_home)
-            settings.home_filesystem_type = home_filesystem
+            if !@root_vol.nil?
+              fs_type = widget_value(:root_fs_type)
+              @root_vol.fs_type = Filesystems::Type.find(fs_type) unless fs_type.nil?
+              @root_vol.snapshots = widget_value(:snapshots)
+            end
+            if !@swap_vol.nil?
+              @swap_vol.adjust_by_ram = widget_value(:enlarge_swap)
+              @swap_vol.fs_type = Y2Storage::Filesystems::Type::SWAP
+            end
+            update_vol_settings!(@home_vol)
+            @other_volumes.each { |vol| update_vol_settings!(vol) }
+          end
+
+          # Update the settings for one volume.
+          # This works only for volumes that have a mount point.
+          #
+          # @param vol [VolumeSpecification]
+          #
+          def update_vol_settings!(vol)
+            return if vol.nil?
+            vol_name = vol.mount_point
+            vol.proposed = widget_value(propose_widget_id(vol_name))
+            if fs_type_user_configurable?(vol)
+              fs_type = widget_value(fs_type_widget_id(vol_name))
+              vol.fs_type = Filesystems::Type.find(fs_type) unless fs_type.nil?
+            end
+          end
+
+          # Find a VolumeSpecification from the proposal settings based on its
+          # mount point.
+          #
+          # @param mount_point [String]
+          #
+          def find_vol(mount_point)
+            return nil if settings.volumes.nil?
+            settings.volumes.find { |vol| vol.mount_point == mount_point }
+          end
+
+          # Find all "other" VolumeSpecifications from the proposal settings,
+          # i.e. volumes other than the standard root, home, swap volumes.
+          #
+          # @return [Array<VolumeSpecifications>]
+          #
+          def find_other_volumes
+            return [] if settings.volumes.nil?
+            settings.volumes.reject { |vol| [@root_vol, @home_vol, @swap_vol].include?(vol) }
           end
         end
       end
--- test/y2storage/dialogs/guided_setup/select_filesystem_ng_test.rb
+++ test/y2storage/dialogs/guided_setup/select_filesystem_ng_test.rb
@@ -22,135 +22,94 @@
 
 require_relative "../../spec_helper.rb"
 require_relative "#{TEST_PATH}/support/guided_setup_context"
+require_relative "#{TEST_PATH}/support/proposal_context"
+require "pp"
 
 describe Y2Storage::Dialogs::GuidedSetup::SelectFilesystem::Ng do
+  include_context "proposal"
   include_context "guided setup requirements"
+
+  let(:scenario) { "empty_hard_disk_gpt_25GiB" }
+  let(:control_file_content) { Yast::XML.XMLToYCPFile(File.join(DATA_PATH, "control_files/volumes_ng", control_file)) }
+  let(:settings) { Y2Storage::ProposalSettings.new_for_current_product }
 
   subject { described_class.new(guided_setup) }
 
   before do
-    # Set some default values
-    filesystem = Y2Storage::Filesystems::Type::BTRFS
-    select_widget(:root_filesystem, filesystem.to_sym)
-    select_widget(:home_filesystem, filesystem.to_sym)
+    Yast::ProductFeatures.Import(control_file_content)
   end
 
-  describe "#run" do
-    it "selects root filesystem from settings" do
-      filesystem = Y2Storage::Filesystems::Type::EXT4
-      settings.root_filesystem_type = filesystem
+  context "In a CaaSP configuration with root and separate /var/lib/docker" do
+    let(:control_file) { "control.CAASP.xml" }
 
-      expect_select(:root_filesystem, filesystem.to_sym)
-      subject.run
-    end
-
-    it "selects home filesystem from settings" do
-      filesystem = Y2Storage::Filesystems::Type::EXT3
-      settings.home_filesystem_type = filesystem
-
-      expect_select(:home_filesystem, filesystem.to_sym)
-      subject.run
-    end
-
-    it "saves settings correctly" do
-      root_filesystem = Y2Storage::Filesystems::Type::BTRFS
-      home_filesystem = Y2Storage::Filesystems::Type::EXT4
-      select_widget(:root_filesystem, root_filesystem.to_sym)
-      select_widget(:home_filesystem, home_filesystem.to_sym)
-      select_widget(:snapshots)
-      select_widget(:separate_home)
-
-      subject.run
-      expect(subject.settings.root_filesystem_type).to eq(root_filesystem)
-      expect(subject.settings.home_filesystem_type).to eq(home_filesystem)
-      expect(subject.settings.use_snapshots).to eq(true)
-      expect(subject.settings.use_separate_home).to eq(true)
-    end
-
-    context "when settings has not snapshots" do
-      before do
-        settings.use_snapshots = false
+    describe "#settings" do
+      it "uses NG settings" do
+        expect(subject.settings.ng_format?).to be(true)
       end
 
-      it "does not select snapshots by default" do
-        expect_not_select(:snapshots)
-        subject.run
+      it "has 2 volumes" do
+        expect(subject.settings.volumes.size).to be == 2
+      end
+
+      it "has a root_vol with Btrfs" do
+        expect(subject.root_vol.mount_point).to eq "/"
+        expect(subject.root_vol.fs_type).to eq Y2Storage::Filesystems::Type::BTRFS
+        expect(subject.root_vol.proposed).to be(true)
+      end
+
+      it "enforces snapshots for the root_vol" do
+        expect(subject.root_vol.snapshots).to be(true)
+        expect(subject.root_vol.snapshots_configurable).to be(false)
+      end
+
+      it "does not have a home_vol" do
+        expect(subject.home_vol).to be nil
+      end
+
+      it "does not have a swap_vol" do
+        expect(subject.home_vol).to be nil
+      end
+
+      it "has one other_vol /var/lib/docker with Btrfs" do
+        expect(subject.other_volumes.size).to be == 1
+        expect(subject.other_volumes.first.mount_point).to eq "/var/lib/docker"
+        expect(subject.other_volumes.first.fs_type).to eq Y2Storage::Filesystems::Type::BTRFS
       end
     end
 
-    context "when settings has snapshots" do
-      before do
-        settings.use_snapshots = true
+    describe "#normalized_id" do
+      it "leaves normal strings untouched" do
+        expect(subject.send(:normalized_id, "foo")).to eq "foo"
       end
 
-      it "selects snapshots by default" do
-        expect_select(:snapshots)
-        subject.run
-      end
-    end
-
-    context "when settings has not separate home" do
-      before do
-        settings.use_separate_home = false
+      it "changes slashes and other special characters to underscores" do
+        expect(subject.send(:normalized_id, "/my/weird/path")).to eq "my_weird_path"
+        expect(subject.send(:normalized_id, "!some$weird*name!")).to eq "some_weird_name"
       end
 
-      it "does not select separate home by default" do
-        expect_not_select(:separate_home)
-        subject.run
+      it "cleans up underscores" do
+        expect(subject.send(:normalized_id, "/$my/weird!!/path?!?")).to eq "my_weird_path"
       end
     end
 
-    context "when settings has separate home" do
-      before do
-        settings.use_separate_home = true
-      end
-
-      it "selects separate home by default" do
-        expect_select(:separate_home)
-        subject.run
+    describe "#propose_widget_id" do
+      it "returns the expected normalized widget IDs" do
+        expect(subject.send(:propose_widget_id, "Home")).to eq :propose_home
+        expect(subject.send(:propose_widget_id, "/home")).to eq :propose_home
+        expect(subject.send(:propose_widget_id, "/var/lib/docker")).to eq :propose_var_lib_docker
       end
     end
 
-    context "when btrfs is selected for root partition" do
-      before do
-        select_widget(:root_filesystem, Y2Storage::Filesystems::Type::BTRFS.to_sym)
-      end
-
-      it "enables snapshots option" do
-        expect_enable(:snapshots)
-        subject.run
+    describe "#fs_type_widget_id" do
+      it "returns the expected normalized widget IDs" do
+        expect(subject.send(:fs_type_widget_id, "Home")).to eq :home_fs_type
+        expect(subject.send(:fs_type_widget_id, "/home")).to eq :home_fs_type
+        expect(subject.send(:fs_type_widget_id, "/var/lib/docker")).to eq :var_lib_docker_fs_type
       end
     end
 
-    context "when other filesystem is selected for root partition" do
-      before do
-        select_widget(:root_filesystem, Y2Storage::Filesystems::Type::EXT4.to_sym)
-      end
-
-      it "disables snapshots option" do
-        expect_disable(:snapshots)
-        subject.run
-      end
-    end
-
-    context "when separate home is selected" do
-      before do
-        select_widget(:separate_home)
-      end
-
-      it "enables filesystem selection for home" do
-        expect_enable(:home_filesystem)
-        subject.run
-      end
-    end
-
-    context "when separate home is not selected" do
-      before do
-        not_select_widget(:separate_home)
-      end
-
-      it "disables filesystem selection for home" do
-        expect_disable(:home_filesystem)
+    describe "#run" do
+      it "does not go up in smoke" do
         subject.run
       end
     end
