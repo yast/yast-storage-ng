6ce6448504888dd6f9486cf806c42406ecbf0a71
José Iván López González <jlopez@suse.com>
2017-12-11 15:38:02 +0000

    Improve add partition button
    - Can work over a partition
    - Label depends on the page

--- test/y2partitioner/widgets/partition_add_button_test.rb
+++ test/y2partitioner/widgets/partition_add_button_test.rb
@@ -26,11 +26,36 @@ require "cwm/rspec"
 require "y2partitioner/widgets/partition_add_button"
 
 describe Y2Partitioner::Widgets::PartitionAddButton do
-  subject { described_class.new(device: device) }
+  subject { described_class.new(device: device, pager: pager) }
 
   let(:device) { nil }
 
+  let(:pager) do
+    instance_double(Y2Partitioner::Widgets::OverviewTreePager, current_page: current_page)
+  end
+
+  let(:current_page) { nil }
+
   include_examples "CWM::PushButton"
+
+  describe "#label" do
+    context "when used in a disks page" do
+      let(:current_page) { Y2Partitioner::Widgets::Pages::Disks.new(nil, nil) }
+
+      it "returns \"Add Partition\"" do
+        expect(subject.label).to include("Add Partition")
+      end
+    end
+
+    context "when used in other pages" do
+      let(:current_page) { Y2Partitioner::Widgets::Pages::MdRaids.new(nil) }
+
+      it "returns \"Add\"" do
+        expect(subject.label).to include("Add")
+        expect(subject.label).to_not include("Partition")
+      end
+    end
+  end
 
   describe "#handle" do
     context "when no device is given" do
@@ -48,20 +73,44 @@ describe Y2Partitioner::Widgets::PartitionAddButton do
 
     context "when a device is given" do
       before do
-        devicegraph_stub("one-empty-disk.yml")
+        devicegraph_stub(scenario)
         allow(action_class).to receive(:new).with(device).and_return(action)
       end
 
-      let(:device) { Y2Partitioner::DeviceGraphs.instance.current.disks.first }
+      let(:scenario) { "one-empty-disk.yml" }
+
+      let(:current_graph) { Y2Partitioner::DeviceGraphs.instance.current }
+
+      let(:device) { current_graph.disks.first }
 
       let(:action) { instance_double(action_class) }
 
       let(:action_class) { Y2Partitioner::Actions::AddPartition }
 
-      it "performs the action for adding a partition" do
-        expect(action_class).to receive(:new).with(device).and_return(action)
-        expect(action).to receive(:run)
-        subject.handle
+      context "and the device is a disk device" do
+        it "performs the action for adding a partition over the disk device" do
+          expect(action_class).to receive(:new).with(device).and_return(action)
+          expect(action).to receive(:run)
+          subject.handle
+        end
+      end
+
+      context "and the device is a partition" do
+        let(:scenario) { "mixed_disks.yml" }
+
+        let(:device) { Y2Storage::Partition.find_by_name(current_graph, "/dev/sda1") }
+
+        let(:disk) { device.partitionable }
+
+        before do
+          allow(device).to receive(:partitionable).and_return(disk)
+        end
+
+        it "performs the action for adding a partition over the partition's disk device" do
+          expect(action_class).to receive(:new).with(disk).and_return(action)
+          expect(action).to receive(:run)
+          subject.handle
+        end
       end
 
       it "returns :redraw if the add action returns :finish" do
