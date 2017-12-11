40f57341681ab793cb93078e1727dcf5f3a85c8b
José Iván López González <jlopez@suse.com>
2017-12-11 15:36:31 +0000

    Improve edit button to directly jump to the device

--- test/y2partitioner/widgets/blk_device_edit_button_test.rb
+++ test/y2partitioner/widgets/blk_device_edit_button_test.rb
@@ -3,46 +3,123 @@ require_relative "../test_helper"
 require "cwm/rspec"
 require "y2partitioner/widgets/blk_device_edit_button"
 
-describe Y2Partitioner::Widgets::BlkDeviceEditButton do
-  let(:device) { Y2Storage::BlkDevice.find_by_name(fake_devicegraph, "/dev/sda2") }
-  let(:sequence) { double("EditBlkDevice", run: :result) }
-
-  before do
-    devicegraph_stub("mixed_disks_btrfs.yml")
-    allow(Y2Partitioner::Actions::EditBlkDevice).to receive(:new).and_return sequence
+RSpec.shared_examples "edit device" do
+  context "and the device is a partition" do
+    let(:scenario) { "mixed_disks.yml" }
+    let(:device_name) { "/dev/sda1" }
+    include_examples "run edit action"
   end
+
+  context "and the device is a Software RAID" do
+    let(:scenario) { "md_raid.xml" }
+    let(:device_name) { "/dev/md/md0" }
+    include_examples "run edit action"
+  end
+
+  context "and the device is a disk" do
+    let(:scenario) { "mixed_disks.yml" }
+    let(:device_name) { "/dev/sda" }
+    include_examples "go to device page"
+  end
+
+  context "and the device is a dasd" do
+    let(:scenario) { "dasd_50GiB.yml" }
+    let(:device_name) { "/dev/sda" }
+    include_examples "go to device page"
+  end
+
+  context "and the device is a multipath" do
+    let(:scenario) { "empty-dasd-and-multipath.xml" }
+    let(:device_name) { "/dev/mapper/36005076305ffc73a00000000000013b4" }
+    include_examples "go to device page"
+  end
+
+  context "and the device is a DM RAID" do
+    let(:scenario) { "empty-dm_raids.xml" }
+    let(:device_name) { "/dev/mapper/isw_ddgdcbibhd_test1" }
+    include_examples "go to device page"
+  end
+
+  context "and the device is a BIOS MD RAID" do
+    let(:scenario) { "md-imsm1-devicegraph.xml" }
+    let(:device_name) { "/dev/md/b" }
+    include_examples "go to device page"
+  end
+end
+
+RSpec.shared_examples "run edit action" do
+  before do
+    allow(Y2Partitioner::Actions::EditBlkDevice).to receive(:new).and_return action
+  end
+
+  let(:action) { instance_double(Y2Partitioner::Actions::EditBlkDevice, run: :finish) }
+
+  it "opens the edit workflow for the device" do
+    expect(Y2Partitioner::Actions::EditBlkDevice).to receive(:new).with(device)
+    button.handle
+  end
+
+  it "returns :redraw if the workflow returns :finish" do
+    allow(action).to receive(:run).and_return :finish
+    expect(button.handle).to eq :redraw
+  end
+
+  it "returns nil if the workflow does not return :finish" do
+    allow(action).to receive(:run).and_return :back
+    expect(button.handle).to be_nil
+  end
+end
+
+RSpec.shared_examples "go to device page" do
+  before do
+    allow(pager).to receive(:device_page).with(device).and_return(page)
+  end
+
+  let(:pager) { instance_double(Y2Partitioner::Widgets::OverviewTreePager) }
+
+  let(:page) { double("page", label: "disk_page") }
+
+  it "jumps to the disk page" do
+    expect(Y2Partitioner::UIState.instance).to receive(:go_to_tree_node).with(page)
+    button.handle
+  end
+
+  it "returns :redraw" do
+    expect(button.handle).to eq :redraw
+  end
+end
+
+describe Y2Partitioner::Widgets::BlkDeviceEditButton do
+  before do
+    devicegraph_stub(scenario)
+  end
+
+  let(:device) { Y2Storage::BlkDevice.find_by_name(fake_devicegraph, device_name) }
+
+  let(:table) { nil }
+
+  let(:pager) { nil }
+
+  let(:scenario) { "mixed_disks.yml" }
+
+  let(:device_name) { "/dev/sda1" }
 
   include_examples "CWM::PushButton"
 
-  context "when defined for a concrete device" do
-    subject(:button) { described_class.new(device: device) }
+  describe "#handle" do
+    context "when defined for a specific device" do
+      subject(:button) { described_class.new(device: device, pager: pager) }
 
-    describe "#handle" do
-      it "opens the edit workflow for the device" do
-        expect(Y2Partitioner::Actions::EditBlkDevice).to receive(:new).with(device)
-        button.handle
-      end
-
-      it "returns :redraw if the workflow returns :finish" do
-        allow(sequence).to receive(:run).and_return :finish
-        expect(button.handle).to eq :redraw
-      end
-
-      it "returns nil if the workflow does not return :finish" do
-        allow(sequence).to receive(:run).and_return :back
-        expect(button.handle).to be_nil
-      end
+      include_examples "edit device"
     end
-  end
 
-  context "when defined for a table" do
-    let(:table) { double("table") }
+    context "when defined for a table" do
+      subject(:button) { described_class.new(table: table, pager: pager) }
 
-    subject(:button) { described_class.new(table: table) }
+      let(:table) { double("table", selected_device: selected_device) }
 
-    describe "#handle" do
       context "when no device is selected in the table" do
-        before { allow(table).to receive(:selected_device).and_return(nil) }
+        let(:selected_device) { nil }
 
         it "shows an error message" do
           expect(Yast::Popup).to receive(:Error)
@@ -60,22 +137,11 @@ describe Y2Partitioner::Widgets::BlkDeviceEditButton do
       end
 
       context "when a device is selected in the table" do
-        before { allow(table).to receive(:selected_device).and_return(device) }
+        let(:device_name) { "/dev/sda1" }
 
-        it "opens the edit workflow for the device" do
-          expect(Y2Partitioner::Actions::EditBlkDevice).to receive(:new).with(device)
-          button.handle
-        end
+        let(:selected_device) { device }
 
-        it "returns :redraw if the workflow returns :finish" do
-          allow(sequence).to receive(:run).and_return :finish
-          expect(button.handle).to eq :redraw
-        end
-
-        it "returns nil if the workflow does not return :finish" do
-          allow(sequence).to receive(:run).and_return :back
-          expect(button.handle).to be_nil
-        end
+        include_examples "edit device"
       end
     end
   end
