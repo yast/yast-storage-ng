274fe1f8c6887255037b03993fa6348c05d960e1
Ancor Gonzalez Sosa <ancor@suse.de>
2018-01-08 12:32:15 +0100

    Avoid exception when running the proposal in a BIOS RAID

---
+++ src/lib/y2storage/disk_device.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+# Copyright (c) [2018] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+module Y2Storage
+  # Mixin for classes that are usually treated like disks by YaST.
+  #
+  # @see Devicegraph::disk_devices
+  #
+  # Includes several methods usually checked by the partitioner, the proposal
+  # and other components. This mixing offers a default conservative
+  # implementation that can be overridden or extended in any of the classes.
+  module DiskDevice
+    # Checks whether it's an USB disk
+    #
+    # @return [Boolean]
+    def usb?
+      false
+    end
+
+    # Checks whether it's in network
+    #
+    # @return [Boolean]
+    def in_network?
+      false
+    end
+  end
+end
