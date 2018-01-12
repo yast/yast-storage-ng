7c559096f4cf808bdcab96c253afbcde4eceb91a
José Iván López González <jlopez@suse.com>
2018-01-12 19:18:56 +0000

    Add SetupError class

---
+++ src/lib/y2storage/setup_error.rb
@@ -0,0 +1,243 @@
+# encoding: utf-8
+
+# Copyright (c) [2018] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "yast"
+require "yast/i18n"
+require "y2storage"
+
+module Y2Storage
+  # Class to represent storage setup error
+  class SetupError
+    include Yast::I18n
+
+    # @return [VolumeSpecification]
+    attr_reader :missing_volume
+
+    # Constructor
+    #
+    # @param message [String, nil] error message
+    # @param missing_volume [VolumeSpecification, nil] missing volume that causes the error
+    def initialize(message: nil, missing_volume: nil)
+      textdomain "storage"
+      @message = message
+      @missing_volume = missing_volume
+    end
+
+    # Error message
+    #
+    # @note If no message was indicated, it can be generated from the missing volume.
+    #
+    # @return [String, nil]
+    def message
+      message = @message
+      message ||= message_for_missing_volume if missing_volume
+      message
+    end
+
+  private
+
+    # Error text for a volume
+    #
+    # @param volume [VolumeSpecification]
+    # @return [String]
+    def message_for_missing_volume
+      if mount_point_info
+        message_with_mount_point
+      else
+        message_without_mount_point
+      end
+    end
+
+    # Error text when the volume has mount point
+    #
+    # @param volume [VolumeSpecification]
+    # @return [String]
+    def message_with_mount_point
+      if partition_id_info && fs_types_info
+        message_with_mount_point_and_partition_id_and_fs
+      elsif partition_id_info
+        message_with_mount_point_and_partition_id
+      elsif fs_types_info
+        message_with_mount_point_and_fs
+      else
+        message_with_mount_point_default
+      end
+    end
+
+    # Error text when the volume does not have mount point
+    #
+    # @param volume [VolumeSpecification]
+    # @return [String]
+    def message_without_mount_point
+      if partition_id_info && fs_types_info
+        message_with_partition_id_and_fs
+      elsif partition_id_info
+        message_with_partition_id
+      elsif fs_types_info
+        message_with_fs
+      else
+        message_without_mount_point_default
+      end
+    end
+
+    # @param volume [VolumeSpecification]
+    # @return [String]
+    def message_with_mount_point_and_partition_id_and_fs
+      # TRANSLATORS: error message, where %{mount_point} is repalaced by a mount point
+      # (e.g., /lib/docker), %{size} by a disk size (e.g., 5 GiB), %{partition_id} by a
+      # partition id (e.g., Linux) and %{fs_types} by a list of filesystem types separated
+      # by comma (e.g., ext2, ext3, ext4).
+      format(
+        "Missing device for %{mount_point} with size equal or bigger than %{size}, " \
+        "partition id %{partition_id} and filesystem %{fs_types}",
+        mount_point:  mount_point_info,
+        size:         size_info,
+        partition_id: partition_id_info,
+        fs_types:     fs_types_info
+      )
+    end
+
+    # @param volume [VolumeSpecification]
+    # @return [String]
+    def message_with_mount_point_and_partition_id
+      # TRANSLATORS: error message, where %{mount_point} is repalaced by a mount point
+      # (e.g., /lib/docker), %{size} by a disk size (e.g., 5 GiB) and %{partition_id}.
+      format(
+        "Missing device for %{mount_point} with size equal or bigger than %{size} " \
+        "and partition id %{partition_id}",
+        mount_point:  mount_point_info,
+        size:         size_info,
+        partition_id: partition_id_info
+      )
+    end
+
+    # @param volume [VolumeSpecification]
+    # @return [String]
+    def message_with_mount_point_and_fs
+      # TRANSLATORS: error message, where %{mount_point} is repalaced by a mount point
+      # (e.g., /lib/docker), %{size} by a disk size (e.g., 5 GiB) and %{fs_types} by a
+      # list of filesystem types separated by comma (e.g., ext2, ext3, ext4).
+      format(
+        "Missing device for %{mount_point} with size equal or bigger than %{size} " \
+        "and filesystem %{fs_types}",
+        mount_point: mount_point_info,
+        size:        size_info,
+        fs_types:    fs_types_info
+      )
+    end
+
+    # @param volume [VolumeSpecification]
+    # @return [String]
+    def message_with_mount_point_default
+      # TRANSLATORS: error message, where %{mount_point} is repalaced by a mount point
+      # (e.g., /lib/docker) and %{size} by a disk size (e.g., 5 GiB).
+      format(
+        "Missing device for %{mount_point} with size equal or bigger than %{size} ",
+        mount_point: mount_point_info,
+        size:        size_info
+      )
+    end
+
+    # @param volume [VolumeSpecification]
+    # @return [String]
+    def message_with_partition_id_and_fs
+      # TRANSLATORS: error message, where %{size} is replaced by a disk size (e.g., 5 GiB),
+      # %{partition_id} by a partition id (e.g., Linux) and %{fs_types} by a list of filesystem
+      # types separated by comma (e.g., ext2, ext3, ext4).
+      format(
+        "Missing device with size equal or bigger than %{size}, " \
+        "partition id %{partition_id} and filesystem %{fs_types}",
+        size:         size_info,
+        partition_id: partition_id_info,
+        fs_types:     fs_types_info
+      )
+    end
+
+    # @param volume [VolumeSpecification]
+    # @return [String]
+    def message_with_partition_id
+      # TRANSLATORS: error message, where %{size} is replaced by a disk size (e.g., 5 GiB) and
+      # %{partition_id} by a partition id (e.g., Linux).
+      format(
+        "Missing device with size equal or bigger than %{size} " \
+        "and partition id %{partition_id}",
+        size:         size_info,
+        partition_id: partition_id_info
+      )
+    end
+
+    # @param volume [VolumeSpecification]
+    # @return [String]
+    def message_with_fs
+      # TRANSLATORS: error message, where %{size} is replaced by a disk size (e.g., 5 GiB) and
+      # %{fs_types} by a list of filesystem types separated by comma (e.g., ext2, ext3, ext4).
+      format(
+        "Missing device with size equal or bigger than %{size} " \
+        "and filesystem %{fs_types}",
+        size:     size_info,
+        fs_types: fs_types_info
+      )
+    end
+
+    # @param volume [VolumeSpecification]
+    # @return [String]
+    def message_without_mount_point_default
+      # TRANSLATORS: error message, where %{size} is replaced by a disk size (e.g., 5 GiB).
+      format(
+        "Missing device with size equal or bigger than %{size}",
+        size: size_info
+      )
+    end
+
+    # Volume mount point to show in the error message
+    #
+    # @param volume [VolumeSpecification]
+    # @return [String, nil]
+    def mount_point_info
+      missing_volume.mount_point
+    end
+
+    # Volume partition id to show in the error message
+    #
+    # @param volume [VolumeSpecification]
+    # @return [Integer, nil]
+    def partition_id_info
+      missing_volume.partition_id
+    end
+
+    # Possible volume fileystem types to show in the error message
+    #
+    # @param volume [VolumeSpecification]
+    # @return [String, nil]
+    def fs_types_info
+      return nil if missing_volume.fs_types.empty?
+      missing_volume.fs_types.map(&:to_s).join(", ")
+    end
+
+    # Volume size to show in the error message
+    #
+    # @param volume [VolumeSpecification]
+    # @return [DiskSize, nil]
+    def size_info
+      missing_volume.min_size
+    end
+  end
+end
---
+++ test/y2storage/setup_error_test.rb
@@ -0,0 +1,189 @@
+#!/usr/bin/env rspec
+# encoding: utf-8
+
+# Copyright (c) [2018] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "spec_helper"
+require "y2storage"
+require "y2storage/setup_error"
+
+describe Y2Storage::SetupError do
+  using Y2Storage::Refinements::SizeCasts
+
+  subject { described_class.new(message: error_message, missing_volume: missing_volume) }
+
+  let(:error_message) { nil }
+
+  let(:missing_volume) { nil }
+
+  let(:volume) do
+    volume = Y2Storage::VolumeSpecification.new({})
+    volume.mount_point = mount_point
+    volume.min_size = min_size
+    volume.fs_types = fs_types
+    volume.partition_id = partition_id
+    volume
+  end
+
+  let(:mount_point) { nil }
+  let(:min_size) { Y2Storage::DiskSize.zero }
+  let(:fs_types) { [] }
+  let(:partition_id) { nil }
+
+  describe "#message" do
+    context "when an error message is given" do
+      let(:error_message) { "an error message" }
+
+      context "and a missing volume is not given" do
+        let(:missing_volume) { nil }
+
+        it "returns the given message" do
+          expect(subject.message).to eq(error_message)
+        end
+      end
+
+      context "and a missing volume is given" do
+        let(:missing_volume) { volume }
+
+        it "returns the given message" do
+          expect(subject.message).to eq(error_message)
+        end
+      end
+    end
+
+    context "when an error message is not given" do
+      let(:error_message) { nil }
+
+      context "and a missing volume is not given" do
+        it "returns nil" do
+          expect(subject.message).to be_nil
+        end
+      end
+
+      context "and a missing volume is given" do
+        let(:missing_volume) { volume }
+
+        context "when the volume has a mount point" do
+          let(:mount_point) { "/mnt/foo" }
+
+          it "generates an error message with mount point data" do
+            expect(subject.message).to match(/device for \/.* /)
+          end
+
+          context "and the volume has a partition id and fs type" do
+            let(:partition_id) { Y2Storage::PartitionId::LINUX }
+
+            let(:fs_types) { [Y2Storage::Filesystems::Type::EXT3] }
+
+            it "generates an error message with partition id and fs type data" do
+              expect(subject.message).to match(/partition id .*/)
+              expect(subject.message).to match(/filesystem .*/)
+            end
+          end
+
+          context "and the volume has a partition id but does not have fs type" do
+            let(:partition_id) { Y2Storage::PartitionId::LINUX }
+
+            let(:fs_types) { [] }
+
+            it "generates an error message with partition id but not fs type data" do
+              expect(subject.message).to match(/partition id .*/)
+              expect(subject.message).to_not match(/filesystem .*/)
+            end
+          end
+
+          context "and the volume does not have a partition id but has a fs type" do
+            let(:partition_id) { nil }
+
+            let(:fs_types) { [Y2Storage::Filesystems::Type::EXT3] }
+
+            it "generates an error message without partition id but with fs type data" do
+              expect(subject.message).to_not match(/partition id .*/)
+              expect(subject.message).to match(/filesystem .*/)
+            end
+          end
+
+          context "and the volume does not have neither partition id nor fs type" do
+            let(:partition_id) { nil }
+
+            let(:fs_types) { [] }
+
+            it "generates a message without partition id neither fs type data" do
+              expect(subject.message).to_not match(/partition id .*/)
+              expect(subject.message).to_not match(/filesystem .*/)
+            end
+          end
+        end
+
+        context "when the volume does not have a mount point" do
+          let(:mount_point) { nil }
+
+          it "generates an error messagge without mount point data" do
+            expect(subject.message).to_not match(/\/.* /)
+          end
+
+          context "and the volume has a partition id and fs type" do
+            let(:partition_id) { Y2Storage::PartitionId::LINUX }
+
+            let(:fs_types) { [Y2Storage::Filesystems::Type::EXT3] }
+
+            it "generates an error message with partition id and fs type data" do
+              expect(subject.message).to match(/partition id .*/)
+              expect(subject.message).to match(/filesystem .*/)
+            end
+          end
+
+          context "and the volume has a partition id but does not have fs type" do
+            let(:partition_id) { Y2Storage::PartitionId::LINUX }
+
+            let(:fs_types) { [] }
+
+            it "generates an error message with partition id but not fs type data" do
+              expect(subject.message).to match(/partition id .*/)
+              expect(subject.message).to_not match(/filesystem .*/)
+            end
+          end
+
+          context "and the volume does not have a partition id but has a fs type" do
+            let(:partition_id) { nil }
+
+            let(:fs_types) { [Y2Storage::Filesystems::Type::EXT3] }
+
+            it "generates an error message without partition id but with fs type data" do
+              expect(subject.message).to_not match(/partition id .*/)
+              expect(subject.message).to match(/filesystem .*/)
+            end
+          end
+
+          context "and the volume does not have neither partition id nor fs type" do
+            let(:partition_id) { nil }
+
+            let(:fs_types) { [] }
+
+            it "generates a message without partition id neither fs type data" do
+              expect(subject.message).to_not match(/partition id .*/)
+              expect(subject.message).to_not match(/filesystem .*/)
+            end
+          end
+        end
+      end
+    end
+  end
+end
