63d2b4ecb1e5661e1b408fee640bb9a6a31f6a57
Ancor Gonzalez Sosa <ancor@suse.de>
2018-02-06 08:11:13 +0100

    Partitioner: page with clickable devicegraphs

---
+++ src/lib/y2partitioner/widgets/pages/device_graph.rb
@@ -0,0 +1,144 @@
+# encoding: utf-8
+
+# Copyright (c) [2018] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "y2partitioner/icons"
+require "y2partitioner/device_graphs"
+require "y2partitioner/widgets/device_graph_with_buttons"
+
+module Y2Partitioner
+  module Widgets
+    module Pages
+      # A page for displaying de device graphs (both current and system) in
+      # interfaces supporting the Graph widget (Qt). Don't use in NCurses.
+      class DeviceGraph < CWM::Page
+        include Yast::I18n
+
+        # Constructor
+        #
+        # @param pager [CWM::TreePager]
+        def initialize(pager)
+          textdomain "storage"
+
+          @pager = pager
+        end
+
+        # @macro seeAbstractWidget
+        def label
+          _("Device Graph")
+        end
+
+        # @macro seeCustomWidget
+        def contents
+          return @contents if @contents
+
+          icon = Icons.small_icon(Icons::GRAPH)
+          @tabs = Tabs.new(current_tab, system_tab)
+          @contents = VBox(
+            Left(
+              HBox(
+                Image(icon, ""),
+                # TRANSLATORS: Heading for the expert partitioner page
+                Heading(_("Device Graphs"))
+              )
+            ),
+            @tabs
+          )
+        end
+
+        # @macro seeAbstractWidget
+        def init
+          # Start always in the first tab
+          @tabs.switch_page(@tabs.initial_page)
+        end
+
+      private
+
+        # @return [CWM::TreePager]
+        attr_reader :pager
+
+        # Tab displaying the current devicegraph with a brief explanation
+        def current_tab
+          DeviceGraphTab.new(
+            _("Planned Devices"),
+            DeviceGraphs.instance.current,
+            # TRANSLATORS: keep lines relatively short. Use \n if needed
+            _(
+              "Final result that will be committed to the system.\n" \
+              "This graph is updated on every user action."
+            ),
+            pager
+          )
+        end
+
+        # Tab displaying the system devicegraph with a brief explanation
+        def system_tab
+          DeviceGraphTab.new(
+            _("Current System Devices"),
+            DeviceGraphs.instance.system,
+            # TRANSLATORS: keep lines relatively short. Use \n if needed
+            _(
+              "Layout of the current system, before any of the scheduled changes.\n" \
+              "This graph is created at startup. Updated if devices are rescanned."
+            ),
+            pager
+          )
+        end
+      end
+
+      # Class to represent every tab in the Device Graph page
+      class DeviceGraphTab < CWM::Tab
+        # @return [String]
+        attr_reader :label
+
+        # Constructor
+        def initialize(label, device_graph, description, pager)
+          @label = label
+          @device_graph = device_graph
+          @widget_id = "#{widget_id}_#{device_graph.object_id}"
+          @description = description
+          @pager = pager
+        end
+
+        # @macro seeCustomWidget
+        def contents
+          return @contents if @contents
+
+          @contents = VBox(
+            Left(Label(description)),
+            DeviceGraphWithButtons.new(device_graph, pager)
+          )
+        end
+
+      private
+
+        # @return [String] explanation to display above the graph
+        attr_reader :description
+
+        # @return [Devicegraph] graph to display
+        attr_reader :device_graph
+
+        # @return [CWM::TreePager] main pager used to jump to the different
+        #   partitioner sections
+        attr_reader :pager
+      end
+    end
+  end
+end
---
+++ src/lib/y2partitioner/widgets/visual_device_graph.rb
@@ -0,0 +1,132 @@
+# encoding: utf-8
+
+# Copyright (c) [2018] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "yast"
+require "cwm"
+require "y2storage"
+require "tempfile"
+
+module Y2Partitioner
+  module Widgets
+    # Widget to display a devicegraph
+    #
+    # It works only in graphical UI (i.e. the Graph widget is available) don't
+    # use it in NCurses.
+    class VisualDeviceGraph < CWM::CustomWidget
+      # Configuration of the Graphviz export (information in the vertices)
+      LABEL_FLAGS = Storage::GraphvizFlags_DISPLAYNAME
+
+      # Configuration of the Graphviz export (tooltips)
+      TOOLTIP_FLAGS = Storage::GraphvizFlags_PRETTY_CLASSNAME |
+        Storage::GraphvizFlags_SIZE | Storage::GraphvizFlags_NAME
+
+      private_constant :LABEL_FLAGS, :TOOLTIP_FLAGS
+
+      # Constructor
+      def initialize(device_graph, pager)
+        textdomain "storage"
+
+        @device_graph = device_graph
+        @pager = pager
+        @widget_id = "#{widget_id}_#{device_graph.object_id}"
+      end
+
+      # @macro seeCustomWidget
+      def contents
+        ReplacePoint(replace_point_id, Empty(graph_id))
+      end
+
+      # @macro seeAbstractWidget
+      def init
+        tmp = Tempfile.new("graph.gv")
+        device_graph.write_graphviz(tmp.path, LABEL_FLAGS, TOOLTIP_FLAGS)
+        content = ReplacePoint(
+          replace_point_id,
+          Yast::Term.new(:Graph, graph_id, Opt(:notify), tmp.path, "dot")
+        )
+        Yast::UI.ReplaceWidget(replace_point_id, content)
+      ensure
+        tmp.close!
+      end
+
+      # @macro seeAbstractWidget
+      def handle
+        node = Yast::UI.QueryWidget(graph_id, :Item)
+        device = device_graph.find_device(node.to_i)
+        return nil unless device
+
+        page = find_target_page(device)
+        return nil unless page
+
+        pager.switch_page(page)
+      end
+
+    private
+
+      # @return [Devicegraph] graph to display
+      attr_reader :device_graph
+
+      # @return [CWM::TreePager] main pager used to jump to the different
+      #   partitioner sections
+      attr_reader :pager
+
+      # Id used for the main widget which is replaced with updated content on
+      # every render (see {#init})
+      def replace_point_id
+        Id(:"#{widget_id}_content")
+      end
+
+      # Id used for the Graph widget and its original empty placeholder
+      def graph_id
+        Id(:"#{widget_id}_graph")
+      end
+
+      # Find the closest partitioner page for the given device
+      #
+      # If there is a page for that device, it returns that page. Otherwise, it
+      # returns the page of the closest ancestor having such page.
+      #
+      # @return [CWM::Page, nil] nil if no appropiate page was found
+      def find_target_page(device)
+        page = pager.device_page(device)
+        return page if page
+
+        parents = device.parents
+        return nil if parents.empty?
+
+        find_ancestor_page(device)
+      end
+
+      # @see #find_target_page
+      #
+      # @return [CWM::Page, nil] nil if no appropiate page was found
+      def find_ancestor_page(device)
+        page = nil
+        device.parents.each do |parent|
+          page = find_target_page(parent)
+          break if page
+        end
+
+        page
+      end
+    end
+  end
+end
