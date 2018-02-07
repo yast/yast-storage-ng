e26513cca993f8d051abc8ab0f098f8abde8e46d
Imobach González Sosa <igonzalezsosa@suse.com>
2018-02-07 16:28:52 +0000

    Updates from code review

--- src/lib/y2storage/proposal/lvm_creator.rb
+++ src/lib/y2storage/proposal/lvm_creator.rb
@@ -102,10 +102,10 @@ module Y2Storage
       # @param devicegraph [Devicegraph] to fetch the partitions
       def assign_physical_volumes(volume_group, part_names, devicegraph)
         partitions = part_names.map { |n| BlkDevice.find_by_name(devicegraph, n) }
-        used_devices = volume_group.lvm_pvs.map(&:blk_device)
+        used_devices = volume_group.lvm_pvs.map(&:plain_blk_device)
         partitions.each do |partition|
-          next if used_devices.include?(partition) # already a PV for this VG
           device = partition.encryption || partition
+          next if used_devices.include?(device) # already a PV for this VG
           device.remove_descendants
           volume_group.add_lvm_pv(device)
         end
