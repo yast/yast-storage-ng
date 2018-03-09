3c5fcb2bad1292e49305529b0b445163252e4bff
Ancor Gonzalez Sosa <ancor@suse.de>
2018-03-09 10:52:05 +0100

    Fix yardoc documentation

--- src/lib/y2storage/planned/can_be_formatted.rb
+++ src/lib/y2storage/planned/can_be_formatted.rb
@@ -39,11 +39,30 @@ module Y2Storage
       # @return [String] UUID to enforce in the filesystem
       attr_accessor :uuid
 
-      # Btrfs subvolume specifications.
+      # @overload subvolumes
+      #   Btrfs subvolume specifications.
       #
-      # @see #subvolumes= which doesn't work exactly as the standard Ruby setter
+      #   @return [Array<SubvolSpecification>]
+      # @overload subvolumes=(list)
+      #   Setter for #subvolumes which always create a local copy of the passed array
       #
-      # @return [Array<SubvolSpecification>]
+      #   When assigning the list of subvolumes, this method automatically creates
+      #   a copy of the original list to avoid the situation in which modifying
+      #   the subvolumes of a planned device ends up modifying the original source
+      #   of such list (bsc#1084213 and bsc#1084261).
+      #
+      #   Take into account this is not a deep copy. Only the collection is
+      #   duplicated, the contained objects are still shared.
+      #
+      #   @example
+      #     planned.subvolumes = my_list
+      #     my_list << a_new_one
+      #     # planned.subvolumes doesn't contain a_new_one now. This is not the
+      #     # most common ruby behavior.
+      #     my_list.first.path = "changed" # This change affects planned.subvolumes
+      #     # because the object is also in that collection (not a deep copy).
+      #
+      #   @param list [Array<SubvolSpecification>]
       attr_reader :subvolumes
 
       # @return [String] Parent for all Btrfs subvolumes (typically "@")
@@ -64,25 +83,7 @@ module Y2Storage
         @snapshots = false
       end
 
-      # Setter for #subvolumes which always create a local copy of the passed array
-      #
-      # When assigning the list of subvolumes, this method automatically creates
-      # a copy of the original list to avoid the situation in which modifying
-      # the subvolumes of a planned device ends up modifying the original source
-      # of such list (bsc#1084213 and bsc#1084261).
-      #
-      # Take into account this is not a deep copy. Only the collection is
-      # duplicated, the contained objects are still shared.
-      #
-      # @example
-      #   planned.subvolumes = my_list
-      #   my_list << a_new_one
-      #   # planned.subvolumes doesn't contain a_new_one now. This is not the
-      #   # most common ruby behavior.
-      #   my_list.first.path = "changed" # This change affects planned.subvolumes
-      #   # because the object is also in that collection (not a deep copy).
-      #
-      # @param list [Array<SubvolSpecification>]
+      # See #subvolumes
       def subvolumes=(list)
         @subvolumes = list.dup
       end
