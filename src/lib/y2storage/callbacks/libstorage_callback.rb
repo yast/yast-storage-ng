9a91b7e4b1db92e9b9ecd3ba5291b94216abc7aa
José Iván López González <jlopez@suse.com>
2018-05-17 14:31:15 +0100

    Add class to read ENV values

--- src/lib/y2storage/callbacks/libstorage_callback.rb
+++ src/lib/y2storage/callbacks/libstorage_callback.rb
@@ -19,8 +19,9 @@
 # To contact SUSE LLC about this file by physical or electronic mail, you may
 # find current contact information at www.suse.com.
 
-require "storage"
 require "yast"
+require "storage"
+require "y2storage/storage_env"
 
 Yast.import "Report"
 Yast.import "Popup"
@@ -148,22 +149,11 @@ module Y2Storage
       # Whether the activation of multipath has been forced via the
       # LIBSTORAGE_MULTIPATH_AUTOSTART boot parameter
       #
-      # See https://en.opensuse.org/SDB:Linuxrc for details and see
-      # bsc#1082542 for an example of scenario in which this is needed.
+      # @see StorageEnv#forced_multipath?
       #
       # @return [Boolean]
       def forced_multipath?
-        # Sort the keys to have a deterministic behavior and to prefer
-        # all-uppercase over the other variants, then do a case insensitive
-        # search
-        key = ENV.keys.sort.find { |k| k.match(/\ALIBSTORAGE_MULTIPATH_AUTOSTART\z/i) }
-        return false unless key
-
-        log.debug "Found key about forcing multipath: #{key.inspect}"
-        value = ENV[key]
-        # Similar to what linuxrc does, also consider the flag activated if the
-        # variable is used with no value or with "1"
-        value.casecmp?("on") || value.empty? || value == "1"
+        StorageEnv.instance.forced_multipath?
       end
     end
   end
---
+++ src/lib/y2storage/storage_env.rb
@@ -0,0 +1,88 @@
+# encoding: utf-8
+
+# Copyright (c) [2018] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "yast"
+require "singleton"
+
+module Y2Storage
+  # Class to read ENV values for storage
+  class StorageEnv
+    include Singleton
+    include Yast::Logger
+
+    ENV_MULTIPATH = "LIBSTORAGE_MULTIPATH_AUTOSTART".freeze
+
+    ENV_BIOS_RAID = "LIBSTORAGE_MDPART".freeze
+
+    private_constant :ENV_MULTIPATH, :ENV_BIOS_RAID
+
+    # Whether the activation of multipath has been forced via the
+    # LIBSTORAGE_MULTIPATH_AUTOSTART boot parameter
+    #
+    # See https://en.opensuse.org/SDB:Linuxrc for details and see
+    # bsc#1082542 for an example of scenario in which this is needed.
+    #
+    # @return [Boolean]
+    def forced_multipath?
+      active?(ENV_MULTIPATH)
+    end
+
+    # Whether the probed MD RAIDS has been forced to be consider BIOS RAIDS
+    # via the LIBSTORAGE_MDPART boot parameter
+    #
+    # See bsc#1092417 for an example of scenario in which this is needed.
+    #
+    # @return [Boolean]
+    def forced_bios_raid?
+      active?(ENV_BIOS_RAID)
+    end
+
+  private
+
+    # Whether the env variable is active
+    #
+    # @param variable [String]
+    # @return [Boolean]
+    def active?(variable)
+      value = read(variable)
+      return false unless value
+
+      # Similar to what linuxrc does, also consider the flag activated if the
+      # variable is used with no value or with "1"
+      value.casecmp?("on") || value.empty? || value == "1"
+    end
+
+    # Read an ENV variable
+    #
+    # @param variable [String]
+    # @return [String, nil]
+    def read(variable)
+      # Sort the keys to have a deterministic behavior and to prefer
+      # all-uppercase over the other variants, then do a case insensitive
+      # search
+      key = ENV.keys.sort.find { |k| k.match(/\A#{variable}\z/i) }
+      return false unless key
+
+      log.debug "Found ENV variable: #{key.inspect}"
+      ENV[key]
+    end
+  end
+end
