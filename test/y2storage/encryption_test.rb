3f46f59b11a940861204c5213421a3861a5ad89e
Ancor Gonzalez Sosa <ancor@suse.de>
2018-06-05 13:09:59 +0200

    Unit tests for Encryption showcasing bsc#1094157

--- test/y2storage/encryption_test.rb
+++ test/y2storage/encryption_test.rb
@@ -26,24 +26,26 @@ require "y2storage/encryption"
 describe Y2Storage::Encryption do
   before do
     fake_scenario(scenario)
-
-    allow(Storage).to receive(:read_simple_etc_crypttab).and_return(storage_entries)
   end
 
   let(:scenario) { "gpt_encryption" }
 
   let(:devicegraph) { Y2Storage::StorageManager.instance.staging }
 
-  let(:fstab_entries) do
-    [
-      crypttab_entry("luks1", device_name, "password", [])
-    ]
-  end
-
-  let(:storage_entries) { fstab_entries.map(&:to_storage_value) }
-
   describe ".use_crypttab_names" do
+    before do
+      allow(Storage).to receive(:read_simple_etc_crypttab).and_return(storage_entries)
+    end
+
     let(:device) { devicegraph.find_by_name(device_name) }
+
+    let(:fstab_entries) do
+      [
+        crypttab_entry("luks1", device_name, "password", [])
+      ]
+    end
+
+    let(:storage_entries) { fstab_entries.map(&:to_storage_value) }
 
     context "when a device indicated in a crypttab entry is an encrypted device" do
       let(:device_name) { "/dev/sda4" }
@@ -89,6 +91,78 @@ describe Y2Storage::Encryption do
         described_class.use_crypttab_names(devicegraph, "path_to_crypttab")
 
         expect(device.encryption).to eq(encryption_before)
+      end
+    end
+  end
+
+  describe ".dm_name_for" do
+    # Helper method to find a partition by number
+    def partition(disk, number)
+      disk.partitions.find { |part| part.number == number }
+    end
+
+    # Helper method to check for collisions in the DeviceMapper names
+    def expect_no_dm_duplicates
+      all_dm_names = devicegraph.blk_devices.map(&:dm_table_name).reject(&:empty?).sort
+      uniq_dm_names = all_dm_names.uniq
+      expect(all_dm_names).to eq uniq_dm_names
+    end
+
+    let(:scenario) { "trivial_lvm_and_other_partitions" }
+    let(:sda) { devicegraph.find_by_name("/dev/sda") }
+
+    context "when the numbers assigned to partitions change" do
+      # Helper method to delete a given partition from a disk
+      def delete_partition(disk, number)
+        disk.partition_table.delete_partition(partition(disk, number))
+      end
+
+      # Helper method to create a partition with an encryption device,
+      # using Encryption.dm_name_for to calculate the name of the latter.
+      def create_encrypted_partition(disk, slot_index)
+        slot = disk.partition_table.unused_partition_slots[slot_index]
+        region = Y2Storage::Region.create(slot.region.start, 8192, slot.region.block_size)
+        part = disk.partition_table.create_partition(
+          slot.name, region, Y2Storage::PartitionType::PRIMARY
+        )
+        enc_name = Y2Storage::Encryption.dm_name_for(part)
+        part.create_encryption(enc_name)
+      end
+
+      before do
+        # Let's free some slots at the beginning of the disk
+        delete_partition(sda, 1)
+        delete_partition(sda, 2)
+      end
+
+      # Regression test for bsc#1094157
+      it "does not generate redundant DeviceMapper names" do
+        # Generate encryption devices for two new partitions sda1 and sda2
+        # at the beginning of the disk
+        create_encrypted_partition(sda, 0)
+        create_encrypted_partition(sda, 0)
+        # Remove the first new partition so the current sda2 becomes sda1
+        delete_partition(sda, 1)
+        # Add a new sda2
+        create_encrypted_partition(sda, 1)
+
+        expect_no_dm_duplicates
+      end
+    end
+
+    context "when the candidate name is already taken" do
+      let(:sda2) { partition(sda, 2) }
+      let(:sda3) { partition(sda, 3) }
+
+      before do
+        # Ensure the first option for the name is already taken
+        enc_name = Y2Storage::Encryption.dm_name_for(sda2)
+        sda3.encryption.dm_table_name = enc_name
+      end
+
+      it "does not generate redundant DeviceMapper names" do
+        sda2.create_encryption(Y2Storage::Encryption.dm_name_for(sda2))
+        expect_no_dm_duplicates
       end
     end
   end
