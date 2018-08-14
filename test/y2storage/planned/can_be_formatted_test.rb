55cb826099f8d3b235283accbd619cc4384f7e67
Imobach González Sosa <igonzalezsosa@suse.com>
2018-08-14 12:49:34 +0100

    Planned::CanBeFormatted set mount_by correctly

--- test/y2storage/planned/can_be_formatted_test.rb
+++ test/y2storage/planned/can_be_formatted_test.rb
@@ -31,30 +31,47 @@ describe Y2Storage::Planned::CanBeFormatted do
     include Y2Storage::Planned::CanBeMounted
     include Y2Storage::Planned::CanBeFormatted
 
+    attr_accessor :reuse
+
     def initialize
       super
       initialize_can_be_formatted
       initialize_can_be_mounted
     end
+
+    def reuse!(devicegraph)
+      dev = Y2Storage::BlkDevice.find_by_name(devicegraph, reuse)
+      reuse_device!(dev)
+    end
   end
 
   subject(:planned) { FormattableDevice.new }
+  let(:blk_device) { Y2Storage::BlkDevice.find_by_name(fake_devicegraph, device_name) }
+  let(:mount_by) { Y2Storage::Filesystems::MountByType::DEVICE }
+  let(:mount_point) { "/" }
+
+  before do
+    fake_scenario("windows-linux-free-pc")
+  end
 
   describe "#format!" do
     let(:filesystem_type) { Y2Storage::Filesystems::Type::BTRFS }
-    let(:blk_device) { Y2Storage::BlkDevice.find_by_name(fake_devicegraph, device_name) }
     let(:device_name) { "/dev/sda2" }
-    let(:mount_point) { "/" }
 
     before do
-      fake_scenario("windows-linux-free-pc")
       planned.filesystem_type = filesystem_type
       planned.mount_point = mount_point
+      planned.mount_by = mount_by
     end
 
     it "creates a filesystem of the given type" do
       planned.format!(blk_device)
       expect(blk_device.filesystem.type).to eq(filesystem_type)
+    end
+
+    it "sets the mount_by option" do
+      planned.format!(blk_device)
+      expect(blk_device.filesystem.mount_by).to eq(mount_by)
     end
 
     context "when filesystem type is not defined" do
@@ -84,6 +101,75 @@ describe Y2Storage::Planned::CanBeFormatted do
         it "does not set the 'ro' option" do
           planned.format!(blk_device)
           expect(blk_device.filesystem.mount_options).to_not include("ro")
+        end
+      end
+    end
+  end
+
+  describe "#reuse_device!" do
+    let(:device_name) { "/dev/sda3" }
+    let(:filesystem_type) { Y2Storage::Filesystems::Type::BTRFS }
+
+    before do
+      planned.reuse = device_name
+    end
+
+    context "when it should be formatted" do
+      before do
+        planned.reformat = true
+        planned.filesystem_type = filesystem_type
+      end
+
+      it "formats the device" do
+        expect(planned).to receive(:format!).with(blk_device)
+        planned.reuse!(fake_devicegraph)
+      end
+    end
+
+    context "when it should not be formatted" do
+      before do
+        planned.reformat = false
+        planned.mount_point = "/old"
+        planned.mount_by = mount_by
+        allow(planned).to receive(:final_device!).and_return(blk_device)
+      end
+
+      context "and a mount point has been set" do
+        it "sets the mount point" do
+          planned.reuse!(fake_devicegraph)
+          expect(blk_device.filesystem.mount_point.path).to eq("/old")
+        end
+
+        context "and a mount_by option is set" do
+          it "sets the mount_by option" do
+            planned.reuse!(fake_devicegraph)
+            expect(blk_device.filesystem.mount_by).to eq(mount_by)
+          end
+        end
+
+        context "and a mount_by option has not been set" do
+          let(:mount_by) { nil }
+
+          it "does not change the mount_by option" do
+            expect { planned.reuse!(fake_devicegraph) }
+              .to_not change { blk_device.filesystem.mount_by }
+          end
+        end
+      end
+
+      context "and a mount point has not been set" do
+        before do
+          planned.mount_point = nil
+        end
+
+        it "does not change the mount point" do
+          expect { planned.reuse!(fake_devicegraph) }
+            .to_not change { blk_device.filesystem.mount_point.path }
+        end
+
+        it "does not change the mount_by option" do
+          expect { planned.reuse!(fake_devicegraph) }
+            .to_not change { blk_device.filesystem.mount_by }
         end
       end
     end
