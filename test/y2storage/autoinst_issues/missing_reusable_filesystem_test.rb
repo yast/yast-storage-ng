ace7a0067ad96fac5b5e5e62b6fd35fc8b5e7e17
Imobach González Sosa <igonzalezsosa@suse.com>
2018-10-02 15:01:55 +0100

    Add a MissingReusableFilesystem AutoYaST issue

---
+++ test/y2storage/autoinst_issues/missing_reusable_filesystem_test.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env rspec
+# encoding: utf-8
+
+# Copyright (c) [2017] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "../../spec_helper"
+require "y2storage/autoinst_profile/partition_section"
+
+describe Y2Storage::AutoinstIssues::MissingReusableFilesystem do
+  subject(:issue) { described_class.new(section) }
+
+  let(:section) do
+    instance_double(Y2Storage::AutoinstProfile::PartitionSection)
+  end
+
+  describe "#message" do
+    it "returns a description of the issue" do
+      expect(issue.message).to eq("Could not find a reusable filesystem")
+    end
+  end
+
+  describe "#severity" do
+    it "returns :error" do
+      expect(issue.severity).to eq(:fatal)
+    end
+  end
+end
