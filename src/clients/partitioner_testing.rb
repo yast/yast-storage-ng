989808a81c813d52fc1852bf7c52ed6295a67531
Martin Vidner <mvidner@suse.cz>
2018-10-17 18:09:56 +0200

    Macro player and profiler

--- src/clients/partitioner_testing.rb
+++ src/clients/partitioner_testing.rb
@@ -38,4 +38,63 @@ when /.xml$/
 else
   raise "Invalid testing parameter #{arg}, expecting foo.yml or foo.xml."
 end
+
+# Run this like this:
+# Y2DIR=../yast-yast2/library/cwm/src/:src \
+#   ruby.ruby2.5 -ryast \
+#   -e 'load "src/lib/y2storage/storage_class_wrapper.rb";
+#       Yast.ui_component="qt";
+#       Yast.import "Arch";
+#       Yast::Arch.s390;
+#       load "/usr/lib/YaST2/bin/y2start"' \
+#   -- \
+#   partitioner_testing -a test/data/devicegraphs/nested_md_raids.yml qt
+# ^ explanation:
+# The things done before 'load y2start' perform initializations
+# (storage, UI, probing) that I want to keep out of the profile data
+
+Yast.import "UI"
+# Monkey Patch! This is a crude but effective UI Macro Player
+events = [
+  { "ID" => :yes }, # we are experts
+  { "ID" => "Y2Partitioner::Widgets::OverviewTree" }, # click the tree
+  # where did we click? see `queries` below
+  { "ID" => "Y2Partitioner::Widgets::OverviewTree" }, # click the tree again
+  { "ID" => :abort } # bye
+]
+old_wfe = Yast::UI.singleton_method(:WaitForEvent)
+Yast::UI.define_singleton_method(:WaitForEvent) do
+  e = events.shift
+  e ||= old_wfe.call
+  Yast.y2milestone "WFE: #{e}"
+  e
+end
+
+Yast::UI.define_singleton_method(:UserInput) do
+  Yast.y2milestone "UI"
+  Yast::UI.WaitForEvent["ID"]
+end
+
+queries = [
+  ["table:device:42"],
+  ["table:device:42"],
+  "Y2Partitioner::Widgets::Pages::MdRaids",
+  ["table:device:42"],
+  ["table:device:42"],
+  "Y2Partitioner::Widgets::Pages::System"
+]
+old_qw = Yast::UI.singleton_method(:QueryWidget)
+Yast::UI.define_singleton_method(:QueryWidget) do |*args|
+  e = queries.shift
+  e ||= old_qw.call(*args)
+  Yast.y2milestone "QW(#{args}): #{e}"
+  e
+end
+
+# Run the rbspy profiler on this.
+# It should be possible to run it from the outside but something in YaST
+# initialization confuses it (cannot find the stack...) so this works better
+system "rbspy record --pid #{$$} --file rbspy.flamegraph --raw-file rbspy.raw.gz --rate 250 &"
+sleep 1
+
 Y2Partitioner::Clients::Main.new.run(allow_commit: false)
