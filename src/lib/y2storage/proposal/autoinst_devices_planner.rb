a4322dfff973a5292ffb87b41122a0fe90f869d2
Imobach González Sosa <igonzalezsosa@suse.com>
2018-10-18 20:57:21 +0100

    Remove an unneeded call to Object#respond_to?

--- src/lib/y2storage/proposal/autoinst_devices_planner.rb
+++ src/lib/y2storage/proposal/autoinst_devices_planner.rb
@@ -109,8 +109,6 @@ module Y2Storage
 
       def remove_shadowed_subvols(planned_devices)
         planned_devices.each do |device|
-          next unless device.respond_to?(:subvolumes)
-
           device.shadowed_subvolumes(planned_devices).each do |subvol|
             # TODO: this should be reported to the user when the shadowed
             # subvolumes was specified in the profile.
