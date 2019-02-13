1c45799728d08825ecfcca71fc99670a38a1b34c
Ancor Gonzalez Sosa <ancor@suse.de>
2019-02-13 10:18:35 +0100

    Guided Setup: render correctly with many disks

--- src/lib/y2storage/dialogs/guided_setup/select_disks.rb
+++ src/lib/y2storage/dialogs/guided_setup/select_disks.rb
@@ -46,31 +46,29 @@ module Y2Storage
 
       protected
 
+        # Maximum number of disks the user is allowed to select
         MAX_DISKS = 3
+
+        # Maximum number of disks that can be displayed at the same time with
+        # the default interface. If that number is exceeded an alternative
+        # interface with scroll is displayed.
+        DISKS_WITHOUT_SCROLL = 10
 
         def dialog_title
           _("Select Hard Disk(s)")
         end
 
         def dialog_content
-          HSquash(
-            VBox(
-              Left(Label(_("Select one or more (max %d) hard disks") % MAX_DISKS)),
-              VSpacing(0.3),
-              *all_disks.map { |d| disk_widget(d) }
-            )
-          )
-        end
-
-        def disk_widget(disk)
-          Left(CheckBox(Id(disk.name), disk_label(disk)))
+          items = all_disks.map { |d| [d.name, disk_label(d)] }
+          label = _("Select one or more (max %d) hard disks") % MAX_DISKS
+          disks_widget.content(label, items)
         end
 
         def initialize_widgets
           default_selected = settings.candidate_devices || []
           default_selected = default_selected.map { |d| analyzer.device_by_name(d) }
           default_selected = all_disks if default_selected.empty?
-          default_selected.first(MAX_DISKS).each { |d| widget_update(d.name, true) }
+          disks_widget.select(default_selected.first(MAX_DISKS).map(&:name))
         end
 
         def update_settings!
@@ -96,11 +94,91 @@ module Y2Storage
         end
 
         def selected_disks
-          all_disks.select { |d| widget_value(d.name) }
+          all_disks.select { |d| disks_widget.selected?(d.name) }
         end
 
         def all_disks
           analyzer.candidate_disks
+        end
+
+        # Widget used to display and select the list of disks
+        #
+        # @return [DisksWidget, ScrollableDisksWidget]
+        def disks_widget
+          @disks_widget ||=
+            if all_disks.size > DISKS_WITHOUT_SCROLL
+              ScrollableDisksWidget.new
+            else
+              DisksWidget.new
+            end
+        end
+
+        # Auxiliary internal class to draw and query the list of disks in the
+        # default case
+        class DisksWidget
+          include Yast::UIShortcuts
+
+          # @see SelectDisks#content
+          #
+          # @param label [String] heading of the widget
+          # @param items [Array<Array<String>>] list of pairs (id, description)
+          def content(label, items)
+            HSquash(
+              VBox(
+                Left(Label(label)),
+                VSpacing(0.3),
+                *items.map { |item| disk_widget(item) }
+              )
+            )
+          end
+
+          # @see SelectDisks#initialize_widgets
+          def select(ids)
+            ids.each { |id| Yast::UI.ChangeWidget(Id(id), :Value, true) }
+          end
+
+          # @see SelectDisks#selected_disks
+          def selected?(id)
+            Yast::UI.QueryWidget(Id(id), :Value)
+          end
+
+        protected
+
+          # @see #content
+          def disk_widget(item)
+            Left(CheckBox(Id(item.first), item.last))
+          end
+        end
+
+        # Auxiliary internal class to draw and query the list of disks when
+        # there are so many disks that the default widget cannot deal with it
+        class ScrollableDisksWidget
+          include Yast::UIShortcuts
+
+          # @see SelectDisks#content
+          #
+          # @param label [String] heading of the widget
+          # @param items [Array<Array<String>>] list of pairs (id, description)
+          def content(label, items)
+            MarginBox(
+              2, 1,
+              MultiSelectionBox(
+                Id(:disks),
+                label,
+                items.map { |item| Item(Id(item.first), item.last) }
+              )
+            )
+          end
+
+          # @see SelectDisks#initialize_widgets
+          def select(ids)
+            Yast::UI.ChangeWidget(Id(:disks), :SelectedItems, ids)
+          end
+
+          # @see SelectDisks#selected_disks
+          def selected?(id)
+            Yast::UI.QueryWidget(Id(:disks), :SelectedItems).include?(id)
+          end
         end
       end
     end
