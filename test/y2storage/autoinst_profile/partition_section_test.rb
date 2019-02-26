61de102c812300594bef6adca82c31ca6fed5048
David Díaz González <dgonzalez@suse.de>
2019-02-26 17:04:37 +0000

    Do not export the partition_type when belongs to GPT
    Related to bsc#1115751.

--- test/y2storage/autoinst_profile/partition_section_test.rb
+++ test/y2storage/autoinst_profile/partition_section_test.rb
@@ -70,6 +70,14 @@ describe Y2Storage::AutoinstProfile::PartitionSection do
         end
       end
 
+      context "when the partition belongs to a GPT partition table" do
+        let(:dev) { device("sdh") }
+
+        it "does not include the partition_type" do
+          expect(section_for("sdh1").partition_type).to be_nil
+        end
+      end
+
       context "when the partition belongs to an MD RAID" do
         let(:dev) { device("sdb1") }
         let(:md) { instance_double(Y2Storage::Md, name: "/dev/md0") }
