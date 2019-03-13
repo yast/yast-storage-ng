42c9896c53784ed5d769db0d336b07d89f878fa2
Stefan Hundhammer <shundhammer@suse.de>
2019-03-13 17:22:33 +0100

    Mount options acl,user_xattr are default (bsc#1122867)

--- src/lib/y2storage/filesystems/type.rb
+++ src/lib/y2storage/filesystems/type.rb
@@ -78,17 +78,16 @@ module Y2Storage
         },
         ext2:     {
           fstab_options:         COMMON_FSTAB_OPTIONS + EXT_FSTAB_OPTIONS,
-          default_fstab_options: ACL_OPTIONS,
           name:                  "Ext2"
         },
         ext3:     {
           fstab_options:         COMMON_FSTAB_OPTIONS + EXT_FSTAB_OPTIONS + ["data="],
-          default_fstab_options: JOURNAL_OPTIONS + ACL_OPTIONS,
+          default_fstab_options: JOURNAL_OPTIONS,
           name:                  "Ext3"
         },
         ext4:     {
           fstab_options:         COMMON_FSTAB_OPTIONS + EXT_FSTAB_OPTIONS + ["data="],
-          default_fstab_options: JOURNAL_OPTIONS + ACL_OPTIONS,
+          default_fstab_options: JOURNAL_OPTIONS,
           name:                  "Ext4"
         },
         hfs:      {
