8c7dd9a6c5bda96b6f882e6eaf4f31594b48abbb
José Iván López González <jlopez@suse.com>
2019-04-10 16:39:59 +0100

    Revert "Export NFS filesystems"
    This reverts commit c0c453dcfa4be409d5d93b46e76e6d29613eaa1a.

--- src/lib/y2storage/autoinst_profile/drive_section.rb
+++ src/lib/y2storage/autoinst_profile/drive_section.rb
@@ -149,7 +149,7 @@ module Y2Storage
       #
       # @param device [BlkDevice] a block device that can be cloned into a
       #   <drive> section, like a disk, a DASD or an LVM volume group.
-      # @return [DriveSection, nil] nil if the device cannot be exported
+      # @return [DriveSection]
       def self.new_from_storage(device)
         result = new
         # So far, only disks (and DASD) are supported
@@ -173,8 +173,6 @@ module Y2Storage
           init_from_vg(device)
         elsif device.is?(:stray_blk_device)
           init_from_stray_blk_device(device)
-        elsif device.is?(:nfs)
-          init_from_nfs(device)
         else
           init_from_disk(device)
         end
@@ -346,16 +344,6 @@ module Y2Storage
         @type = :CT_DISK
         @device = device.name
         @enabled_snapshots = enabled_snapshots?([device.filesystem]) if device.filesystem
-        @use = "all"
-        @disklabel = "none"
-        @partitions = [PartitionSection.new_from_storage(device)]
-
-        true
-      end
-
-      def init_from_nfs(device)
-        @type = :CT_NFS
-        @device = device.share
         @use = "all"
         @disklabel = "none"
         @partitions = [PartitionSection.new_from_storage(device)]
