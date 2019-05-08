438fe8b7e958b6e56ff9eb7358634f15178dd1ff
Ancor Gonzalez Sosa <ancor@suse.de>
2019-05-08 10:29:35 +0200

    Preparation commit: DeleteDevice easier to specialize

--- src/lib/y2partitioner/actions/delete_device.rb
+++ src/lib/y2partitioner/actions/delete_device.rb
@@ -181,7 +181,7 @@ module Y2Partitioner
         # TRANSLATORS: Note added to the dialog for trying to unmount a device
         note = _("It cannot be deleted while mounted.")
 
-        immediate_unmount(controller.committed_device, note: note)
+        immediate_unmount(committed_device, note: note)
       end
 
       # Whether it is necessary to try unmount (i.e., when deleting a mounted block device that
@@ -189,8 +189,23 @@ module Y2Partitioner
       #
       # @return [Boolean]
       def need_try_unmount?
-        return false unless device.is?(:blk_device)
+        return false unless device.is?(:blk_device, :blk_filesystem)
 
+        committed_device_mounted?
+      end
+
+      # Device taken from the system devicegraph
+      #
+      # @return [Y2Storage::Device, nil] nil if the device does not exist on disk yet.
+      def committed_device
+        controller.committed_device
+      end
+
+      # Whether {#committed_device} exists and is mounted, according to the
+      # system devicegraph
+      #
+      # @return [Boolean]
+      def committed_device_mounted?
         controller.mounted_committed_filesystem?
       end
     end
