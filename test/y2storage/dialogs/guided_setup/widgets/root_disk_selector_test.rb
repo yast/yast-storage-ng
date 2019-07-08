4a760e114383b152275eb439030ec670c1313457
David Díaz González <dgonzalez@suse.de>
2019-07-08 10:40:17 +0100

    Fix test of RootDiskSelector widget
    Which only make sense for :auto allocate_volume_mode

--- test/y2storage/dialogs/guided_setup/widgets/root_disk_selector_test.rb
+++ test/y2storage/dialogs/guided_setup/widgets/root_disk_selector_test.rb
@@ -27,6 +27,7 @@ describe Y2Storage::Dialogs::GuidedSetup::Widgets::RootDiskSelector do
 
   before do
     fake_scenario(scenario)
+    allow(settings).to receive(:allocate_volume_mode).and_return(:auto)
   end
 
   subject { described_class.new(widget_id, settings, candidate_disks: candidate_disks) }
