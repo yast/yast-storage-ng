6485477cfd882c6c6af69a9ceea3034c95199477
Arvin Schnell <aschnell@suse.de>
2019-07-30 10:54:42 +0200

    write unit test more rspec like

--- test/y2partitioner/blk_device_restorer_test.rb
+++ test/y2partitioner/blk_device_restorer_test.rb
@@ -30,14 +30,32 @@ describe Y2Partitioner::BlkDeviceRestorer do
     devicegraph_stub(devicegraph)
   end
 
-  describe "#restore_from_checkpoint" do
-    it "does not crash when restoring device used by filesystem itself" do
-      device = Y2Partitioner::DeviceGraphs.instance.current.find_by_name("/dev/sda1")
-      filesystem = device.direct_blk_filesystem
+  subject { described_class.new(device) }
 
-      # same sequence as in Controllers::BtrfsDevices.remove_device
-      filesystem.remove_device(device)
-      Y2Partitioner::BlkDeviceRestorer.new(device).restore_from_checkpoint
+  let(:device) { Y2Partitioner::DeviceGraphs.instance.current.find_by_name(device_name) }
+
+  describe "#restore_from_checkpoint" do
+    let(:device_name) { "/dev/sda1" }
+
+    context "when restoring a device used by filesystem itself" do
+      let(:filesystem) { device.direct_blk_filesystem }
+
+      before do
+        # same sequence as in Controllers::BtrfsDevices.remove_device
+        filesystem.remove_device(device)
+      end
+
+      it "does not raise an exception" do
+        expect { subject.restore_from_checkpoint }.to_not raise_error
+      end
+
+      it "does not remove the filesystem" do
+        sid = filesystem.sid
+
+        subject.restore_from_checkpoint
+
+        expect(Y2Partitioner::DeviceGraphs.instance.current.find_device(sid)).to_not be_nil
+      end
     end
   end
 
