93a437dd6dcf1262cfc90d3cbb03099041ea0053
Steffen Winterfeldt <wfeldt@opensuse.org>
2019-08-29 15:05:35 +0200

    bind-mount /run from inst-sys to target system during install (bsc#1136463)
    That's basically bnc#717321 but for storage-ng.

--- src/lib/y2storage/clients/inst_prepdisk.rb
+++ src/lib/y2storage/clients/inst_prepdisk.rb
@@ -68,6 +68,7 @@ module Y2Storage
         mount_in_target("/proc", "proc", "-t proc")
         mount_in_target("/sys", "sysfs", "-t sysfs")
         mount_in_target(EFIVARS_PATH, "efivarfs", "-t efivarfs") if File.exist?(EFIVARS_PATH)
+        mount_in_target("/run", "/run", "--bind")
 
         true
       end
@@ -80,6 +81,9 @@ module Y2Storage
             raise ".target.mkdir failed"
           end
         end
+
+        log.info "Cmd: mount #{options} #{device} #{target_path}"
+
         if !SCR.Execute(path(".target.mount"), [device, target_path], options)
           raise ".target.mount failed"
         end
