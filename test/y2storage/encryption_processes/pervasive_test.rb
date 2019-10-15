e6071b9ce250329464387b4a32e6e071e0be45d5
Imobach González Sosa <igonzalezsosa@suse.com>
2019-10-15 15:33:21 +0100

    Extend EncryptionProcesses::Pervasive unit test

--- test/y2storage/encryption_processes/pervasive_test.rb
+++ test/y2storage/encryption_processes/pervasive_test.rb
@@ -29,20 +29,44 @@ describe Y2Storage::EncryptionProcesses::Pervasive do
   let(:devicegraph) { Y2Partitioner::DeviceGraphs.instance.current }
   let(:blk_device) { Y2Storage::BlkDevice.find_by_name(devicegraph, "/dev/sda") }
   let(:dm_name) { "cr_sda" }
+  let(:zkey_cryptsetup) do
+    "cryptsetup luksFormat --foo bar --dummy /dev/dasdc1\n" \
+      "zkey-cryptsetup setvp --volumes /dev/dasdc1\n" \
+      "third-command"
+  end
+
+  let(:secure_key) { nil }
 
   before do
     devicegraph_stub("empty_hard_disk_50GiB.yml")
+    allow(Yast::Execute).to receive(:locally)
+      .with(/zkey/, "cryptsetup", "--volumes", "/dev/dasdc1", anything)
+      .and_return(zkey_cryptsetup)
+    allow(Y2Storage::EncryptionProcesses::SecureKey).to receive(:for_device)
+      .and_return(secure_key)
   end
 
   describe "#create_device" do
     it "returns an encryption device" do
       encryption = process.create_device(blk_device, dm_name)
       expect(encryption.is?(:encryption)).to eq(true)
+      expect(encryption.dm_table_name).to eq("cr_sda")
     end
 
     it "creates an luks2 encryption device for given block device" do
       encryption = process.create_device(blk_device, dm_name)
       expect(encryption.type).to eq(Y2Storage::EncryptionType::LUKS2)
+    end
+
+    context "when a secure key for the device was found" do
+      let(:secure_key) do
+        instance_double(Y2Storage::EncryptionProcesses::SecureKey, dm_name: "cr_custom")
+      end
+
+      it "uses the dm name from the key" do
+        encryption = process.create_device(blk_device, dm_name)
+        expect(encryption.dm_table_name).to eq("cr_custom")
+      end
     end
 
     it "does not set any specific encryption option" do
@@ -53,6 +77,68 @@ describe Y2Storage::EncryptionProcesses::Pervasive do
     it "does not set any specific open option" do
       encryption = subject.create_device(blk_device, dm_name)
       expect(encryption.open_options).to be_empty
+    end
+  end
+
+  describe "#pre_commit" do
+    let(:encryption) { subject.create_device(blk_device, dm_name) }
+
+    let(:secure_key) { nil }
+
+    let(:generated_key) do
+      instance_double(Y2Storage::EncryptionProcesses::SecureKey,
+        plain_name: "/dev/dasdc1", dm_name: "cr_1", name: "secure_xtskey1")
+    end
+
+    before do
+      allow(Y2Storage::EncryptionProcesses::SecureKey).to receive(:generate)
+        .and_return(generated_key)
+    end
+
+    it "generates a new secure key for the device" do
+      expect(Y2Storage::EncryptionProcesses::SecureKey).to receive(:generate)
+        .with("YaST_cr_sda", volumes: [encryption]).and_return(generated_key)
+      subject.pre_commit(encryption)
+    end
+
+    context "when a secure key for the device was found" do
+      let(:secure_key) { generated_key }
+
+      it "does not generate a new secure key" do
+        expect(Y2Storage::EncryptionProcesses::SecureKey).to_not receive(:generate)
+        subject.pre_commit(encryption)
+      end
+    end
+
+    it "sets LUKS format options" do
+      subject.pre_commit(encryption)
+      expect(encryption.format_options).to include("--foo bar --dummy --pbkdf pbkdf2")
+    end
+  end
+
+  describe "#post_commit" do
+    let(:encryption) { subject.create_device(blk_device, dm_name) }
+
+    let(:secure_key) do
+      instance_double(Y2Storage::EncryptionProcesses::SecureKey,
+        plain_name: "/dev/dasdc1", dm_name: "cr_1", name: "secure_xtskey1")
+    end
+
+    before do
+      subject.pre_commit(encryption)
+    end
+
+    it "executes commands reported by zkey cryptsetup skipping the first one" do
+      expect(Yast::Execute).to receive(:locally).with(/zkey-cryptsetup/, any_args)
+      expect(Yast::Execute).to receive(:locally).with("third-command")
+      subject.post_commit(encryption)
+    end
+
+    it "adds the --key-file option to the setvp command" do
+      allow(Yast::Execute).to receive(:locally)
+      expect(Yast::Execute).to receive(:locally)
+        .with(/zkey-cryptsetup/, "setvp", "--volumes", "/dev/dasdc1", "--key-file", "-", any_args)
+      subject.post_commit(encryption)
     end
   end
 end
