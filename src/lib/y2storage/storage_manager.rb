9a75ff5eedd800a1a3f451e8c3786b2fb58e37cf
Ancor Gonzalez Sosa <ancor@suse.de>
2020-04-03 16:15:18 +0200

    Refresh the system devicegraph when reprobing (bsc#1168325)

--- src/lib/y2storage/storage_manager.rb
+++ src/lib/y2storage/storage_manager.rb
@@ -386,6 +386,7 @@ module Y2Storage
     def reset_probed
       @raw_probed_graph = nil
       @probed_graph = nil
+      @system_graph = nil
       @probed_disk_analyzer = nil
       @committed = false
       Y2Storage::HWInfoReader.instance.reset
