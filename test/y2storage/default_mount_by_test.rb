f4e29e5528eaf123a3a4d273ce1de79342b78f1a
Steffen Winterfeldt <wfeldt@opensuse.org>
2020-05-14 15:19:34 +0200

    added unit tests

--- test/y2storage/default_mount_by_test.rb
+++ test/y2storage/default_mount_by_test.rb
@@ -22,23 +22,75 @@ require_relative "../spec_helper"
 require "y2storage"
 
 describe "default mount_by when creating a mount point" do
-  before { fake_scenario(scenario) }
-
-  # This is a modified version of the scenario of the bug to include more cases.
-  # The original report was about the installer using the device name to mount
-  # those devices without an udev path (mounting by path is the default in s390).
-  let(:scenario) { "bug_1151075.xml" }
+  before do
+    fake_scenario(scenario)
+    conf = Y2Storage::StorageManager.instance.configuration
+    conf.default_mount_by = mount_by_type
+  end
 
   let(:blk_device) { Y2Storage::BlkDevice.find_by_name(fake_devicegraph, dev_name) }
   subject(:filesystem) { blk_device.filesystem }
 
-  context "with PATH being the default mount_by" do
-    before do
-      conf = Y2Storage::StorageManager.instance.configuration
-      conf.default_mount_by = Y2Storage::Filesystems::MountByType::PATH
+  describe "Mountable#create_mount_point" do
+    let(:scenario) { "encrypted_probed_partition.xml" }
+    let(:dev_name) { "/dev/mapper/cr_sda1" }
+
+    context "with DEVICE being the default mount_by" do
+      let(:mount_by_type) { Y2Storage::Filesystems::MountByType::DEVICE }
+
+      context "mounting an encrypted partition" do
+        it "mounts by device" do
+          mp = filesystem.create_mount_point("/foo")
+          expect(mp.mount_by.is?(:device)).to eq true
+        end
+
+        it "uses device name in crypttab" do
+          mp = filesystem.create_mount_point("/foo")
+          expect(mp.filesystem.blk_devices.first.mount_by.is?(:device)).to eq true
+        end
+      end
     end
 
-    describe "Mountable#create_mount_point" do
+    context "with UUID being the default mount_by" do
+      let(:mount_by_type) { Y2Storage::Filesystems::MountByType::UUID }
+
+      context "mounting an encrypted partition" do
+        it "mounts by uuid" do
+          mp = filesystem.create_mount_point("/foo")
+          expect(mp.mount_by.is?(:uuid)).to eq true
+        end
+
+        it "uses uuid in crypttab" do
+          mp = filesystem.create_mount_point("/foo")
+          expect(mp.filesystem.blk_devices.first.mount_by.is?(:uuid)).to eq true
+        end
+      end
+    end
+
+    context "with LABEL being the default mount_by" do
+      let(:mount_by_type) { Y2Storage::Filesystems::MountByType::LABEL }
+
+      context "mounting an encrypted partition" do
+        it "mounts by label" do
+          mp = filesystem.create_mount_point("/foo")
+          expect(mp.mount_by.is?(:label)).to eq true
+        end
+
+        it "uses uuid in crypttab" do
+          mp = filesystem.create_mount_point("/foo")
+          expect(mp.filesystem.blk_devices.first.mount_by.is?(:uuid)).to eq true
+        end
+      end
+    end
+
+    context "with PATH being the default mount_by" do
+      let(:mount_by_type) { Y2Storage::Filesystems::MountByType::PATH }
+
+      # This is a modified version of the scenario of the bug to include more cases.
+      # The original report was about the installer using the device name to mount
+      # those devices without an udev path (mounting by path is the default in s390).
+      let(:scenario) { "bug_1151075.xml" }
+
       context "mounting a partition with udev path" do
         let(:dev_name) { "/dev/nvme1n1p1" }
 
