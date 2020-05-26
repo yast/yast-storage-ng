2ac6d33f4f144df9072d45c223d685e03941aa7f
Stefan Schubert <schubi@suse.de>
2020-05-26 16:01:23 +0200

    fixed testcase

--- test/y2storage/autoinst_issues/issues_presenter_test.rb
+++ test/y2storage/autoinst_issues/issues_presenter_test.rb
@@ -18,7 +18,7 @@
 # To contact SUSE LLC about this file by physical or electronic mail, you may
 # find current contact information at www.suse.com.
 
-require_relative "../test_helper"
+require_relative "../../spec_helper"
 require "y2storage"
 require "y2storage/autoinst_issues"
 require "y2storage/autoinst_profile"
