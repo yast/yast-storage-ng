9eb719f24c3b72befacb7ff50ea7578b8750c5ac
Stefan Hundhammer <shundhammer@suse.de>
2020-08-20 15:06:02 +0200

    Integrate menu bar into CWM structure

--- src/lib/y2partitioner/widgets/main_menu_bar.rb
+++ src/lib/y2partitioner/widgets/main_menu_bar.rb
@@ -17,9 +17,46 @@
 # To contact SUSE LLC about this file by physical or electronic mail, you may
 # find current contact information at www.suse.com.
 
+require "yast"
+require "cwm"
+
 module Y2Partitioner
-  module Dialogs
-    module MainMenus
+  module Widgets
+    # Main menu bar of the partitioner
+    class MainMenuBar < CWM::CustomWidget
+      include Yast::Logger
+
+      # Constructor
+      def initialize
+        textdomain "storage"
+        self.handle_all_events = true
+        super
+      end
+
+      # Widget contents
+      def contents
+        @contents ||= MenuBar(Id(:menu_bar), main_menus)
+      end
+
+      # Event handler for the main menu
+      #
+      # @param event [Hash] UI event
+      # @return [:redraw, nil] :redraw when some configuration client was
+      #   executed; nil otherwise.
+      def handle(event)
+        return nil unless menu_event?(event)
+        id = event["ID"]
+        log.info("Handling menu event: #{id}")
+        nil
+      end
+
+      # Check if a UI event is a menu event
+      def menu_event?(event)
+        event["EventType"] == "MenuEvent"
+      end
+
+      private
+
       def main_menus
         textdomain "storage"
         [
@@ -27,6 +64,7 @@ module Y2Partitioner
           Menu(_("&System"), system_menu),
           Menu(_("&Edit"), edit_menu),
           Menu(_("&View"), view_menu),
+          Menu(_("&Configure"), configure_menu),
           Menu(_("&Options"), options_menu)
         ].freeze
       end
@@ -34,12 +72,12 @@ module Y2Partitioner
       def system_menu
         [
           # TRANSLATORS: Menu items in the partitioner
-          Item(Id(:rescan_devices), _("R&escan Devices")),
-          Item(Id(:settings), _("Se&ttings...")),
+          Item(Id("Y2Partitioner::Widgets::RescanDevicesButton"), _("R&escan Devices")),
+          Item(Id(_("Settings")), _("Se&ttings...")),
           Item("---"),
-          Item(Id(:abort), _("&Abort (Without Saving)")),
+          Item(Id(:abort), _("&Abort (Abandoning Changes)")),
           Item("---"),
-          Item(Id(:finish), _("&Finish (Save and Exit)"))
+          Item(Id(:next), _("&Finish (Save and Exit)"))
         ].freeze
       end
 
@@ -59,33 +97,25 @@ module Y2Partitioner
       def view_menu
         [
           # TRANSLATORS: Menu items in the partitioner
-          Item(Id(:view_device_graphs), _("Device &Graphs...")),
-          Item(Id(:view_summary), _("Installation &Summary..."))
+          Item(Id(_("Device Graphs")), _("Device &Graphs...")),
+          Item(Id(_("Installation Summary")), _("Installation &Summary..."))
+        ].freeze
+      end
+
+      def configure_menu
+        [
+          # TRANSLATORS: Menu items in the partitioner
+          Item(Id(:CryptAction), _("Provide &Crypt Passwords...")),
+          Item(Id(:IscsiAction), _("Configure &iSCSI...")),
+          Item(Id(:FcoeAction), _("Configure &FCoE..."))
         ].freeze
       end
 
       def options_menu
         [
           # TRANSLATORS: Menu items in the partitioner
-          Item(Id(:crypt_passwords), _("Provide &Crypt Passwords...")),
-          Menu(_("Partition &Table"), partition_table_menu),
-          Menu(_("&Network Storage"), network_storage_menu)
-        ].freeze
-      end
-
-      def partition_table_menu
-        [
-          # TRANSLATORS: Menu items in the partitioner
           Item(Id(:create_partition_table), _("Create New Partition &Table...")),
           Item(Id(:clone_partitions), _("&Clone Partitions to Other Devices..."))
-        ].freeze
-      end
-
-      def network_storage_menu
-        [
-          # TRANSLATORS: Menu items in the partitioner
-          Item(Id(:configure_iscsi), _("Configure &iSCSI...")),
-          Item(Id(:configure_fcoe), _("Configure &FCoE..."))
         ].freeze
       end
     end
