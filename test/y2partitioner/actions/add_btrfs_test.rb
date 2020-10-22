ab9c321ce1e528eb485fe38d4470d7a9c21e488a
Ancor Gonzalez Sosa <ancor@suse.de>
2020-10-22 11:46:51 +0200

    Partitioner: select correct row after creating a Btrfs

--- test/y2partitioner/actions/add_btrfs_test.rb
+++ test/y2partitioner/actions/add_btrfs_test.rb
@@ -32,11 +32,33 @@ describe Y2Partitioner::Actions::AddBtrfs do
   end
 
   subject(:sequence) { described_class.new }
-  let(:current_graph) { Y2Partitioner::DeviceGraphs.instance.current }
+
+  # Number of Btrfs filesystems in the current devicegraph
+  #
+  # Using "let" wouldn't be an option here. To test the real transaction we always want to
+  # check in the devicegraph that is current in the precise moment we ask, with no caching.
+  def btrfs_count
+    Y2Partitioner::DeviceGraphs.instance.current.btrfs_filesystems.size
+  end
 
   describe "#run" do
+    let(:controller) { Y2Partitioner::Actions::Controllers::BtrfsDevices.new }
+
     before do
       allow(Yast2::Popup).to receive(:show)
+
+      allow(Y2Partitioner::Actions::Controllers::Filesystem).to receive(:new)
+
+      allow(Y2Partitioner::Actions::Controllers::BtrfsDevices).to receive(:new)
+        .and_return(controller)
+
+      allow(Y2Partitioner::Dialogs::BtrfsDevices).to receive(:run) do
+        # If this dialog returns :next, it has for sure added at least one device
+        device = Y2Partitioner::DeviceGraphs.instance.current.find_by_name("/dev/sda3")
+        controller.add_device(device)
+
+        :next
+      end
     end
 
     context "when there are not enough available devices" do
@@ -57,13 +79,7 @@ describe Y2Partitioner::Actions::AddBtrfs do
       let(:devicegraph) { "complex-lvm-encrypt.yml" }
 
       context "if the user goes forward through all the dialogs" do
-        let(:controller) { Y2Partitioner::Actions::Controllers::BtrfsDevices.new }
-
         before do
-          allow(Y2Partitioner::Actions::Controllers::Filesystem).to receive(:new)
-          allow_any_instance_of(Y2Partitioner::Actions::Controllers::BtrfsDevices).to receive(:new)
-            .and_return(controller)
-          allow(Y2Partitioner::Dialogs::BtrfsDevices).to receive(:run).and_return :next
           allow(Y2Partitioner::Dialogs::BtrfsOptions).to receive(:run).and_return :next
         end
 
@@ -72,20 +88,14 @@ describe Y2Partitioner::Actions::AddBtrfs do
         end
 
         it "creates a new Btrfs filesytem" do
-          expect(current_graph.btrfs_filesystems.size).to eq 0
-
-          device = current_graph.find_by_name("/dev/sda3")
-          controller.add_device(device)
+          expect(btrfs_count).to eq 0
           sequence.run
-
-          expect(current_graph.btrfs_filesystems.size).to eq 1
+          expect(btrfs_count).to eq 1
         end
       end
 
       context "if the user aborts the process at some point" do
         before do
-          allow(Y2Partitioner::Actions::Controllers::Filesystem).to receive(:new)
-          allow(Y2Partitioner::Dialogs::BtrfsDevices).to receive(:run).and_return :next
           allow(Y2Partitioner::Dialogs::BtrfsOptions).to receive(:run).and_return :abort
         end
 
@@ -94,7 +104,9 @@ describe Y2Partitioner::Actions::AddBtrfs do
         end
 
         it "does not create a new Btrfs filesystem in the devicegraph" do
-          expect { sequence.run }.to_not(change { current_graph.btrfs_filesystems })
+          expect(btrfs_count).to eq 0
+          sequence.run
+          expect(btrfs_count).to eq 0
         end
       end
     end
