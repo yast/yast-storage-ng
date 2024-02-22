5788f2a5b40b463fbed3b8be451a3bed79a2e448
Ancor Gonzalez Sosa <ancor@suse.de>
2024-02-22 15:14:50 +0100

    [Proposal] Extend Agama basis tests

--- test/y2storage/proposal_agama_basis_test.rb
+++ test/y2storage/proposal_agama_basis_test.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env rspec
-# Copyright (c) [2023] SUSE LLC
+
+# Copyright (c) [2023-2024] SUSE LLC
 #
 # All Rights Reserved.
 #
@@ -33,7 +34,7 @@ describe Y2Storage::MinGuidedProposal do
     let(:settings_format) { :ng }
     let(:separate_home) { true }
     let(:control_file_content) do
-      { "partitioning" => { "proposal" => { "windows_delete_mode" => :none }, "volumes" => volumes } }
+      { "partitioning" => { "proposal" => proposal_settings, "volumes" => volumes } }
     end
 
     # Several disks fully used by Windows partitions
@@ -72,140 +73,163 @@ describe Y2Storage::MinGuidedProposal do
       settings.separate_vgs = true
     end
 
-    context "when ProposalSettings#lvm is set to false" do
-      let(:lvm) { false }
+    RSpec.shared_examples "volume combinations" do
+      context "when ProposalSettings#lvm is set to false" do
+        let(:lvm) { false }
 
-      context "if no alternative disks are specified for any of the volumes" do
-        let(:expected_scenario_filename) { "agama-basis-single_disk" }
+        context "if no alternative disks are specified for any of the volumes" do
+          let(:expected_scenario_filename) { "agama-basis-single_disk" }
 
-        # Creates all partitions in the target disk and leaves everything else untouched
-        include_examples "proposed layout"
-      end
-
-      context "if some partitions are assigned to different disks" do
-        let(:expected_scenario_filename) { "agama-basis-distributed" }
-
-        before do
-          settings.volumes.each do |vol|
-            vol.device = "/dev/sda" if vol.mount_point == "/home"
-            vol.device = "/dev/sdc" if vol.mount_point == "/srv"
-          end
+          # Creates all partitions in the target disk and leaves everything else untouched
+          include_examples "proposed layout"
         end
 
-        # Creates default partitions in the target disk and special ones at sda and sdc as requested
-        include_examples "proposed layout"
-
-        context "if some volume must be created at a dedicated volume group" do
-          let(:expected_scenario_filename) { "agama-basis-distributed_with_lvm" }
+        context "if some partitions are assigned to different disks" do
+          let(:expected_scenario_filename) { "agama-basis-distributed" }
 
           before do
-            settings.volumes.find { |v| v.mount_point == "/home" }.separate_vg_name = "home"
+            settings.volumes.each do |vol|
+              vol.device = "/dev/sda" if vol.mount_point == "/home"
+              vol.device = "/dev/sdc" if vol.mount_point == "/srv"
+            end
           end
 
-          # Like the previous one but with an extra volume group
+          # Creates default partitions in the target disk and special ones at sda and sdc as requested
+          include_examples "proposed layout"
+
+          context "if some volume must be created at a dedicated volume group" do
+            let(:expected_scenario_filename) { "agama-basis-distributed_with_lvm" }
+
+            before do
+              settings.volumes.find { |v| v.mount_point == "/home" }.separate_vg_name = "home"
+            end
+
+            # Like the previous one but with an extra volume group
+            include_examples "proposed layout"
+          end
+        end
+
+        context "if all partitions (even the root one) are assigned to a different disk" do
+          let(:expected_scenario_filename) { "agama-basis-separate_boot" }
+
+          before { settings.volumes.each { |v| v.device = "/dev/sda" } }
+
+          # Creates all partitions at sda except the bios_boot one (at sdb)
           include_examples "proposed layout"
         end
       end
 
-      context "if all partitions (even the root one) are assigned to a different disk" do
-        let(:expected_scenario_filename) { "agama-basis-separate_boot" }
+      context "when ProposalSettings#lvm is set to true" do
+        let(:lvm) { true }
 
-        before { settings.volumes.each { |v| v.device = "/dev/sda" } }
+        context "if all volumes must be located in the system VG" do
+          before do
+            settings.volumes.each { |v| v.separate_vg_name = nil }
+          end
 
-        # Creates all partitions at sda except the bios_boot one (at sdb)
-        include_examples "proposed layout"
-      end
-    end
-
-    context "when ProposalSettings#lvm is set to true" do
-      let(:lvm) { true }
-
-      context "if all volumes must be located in the system VG" do
-        before do
-          settings.volumes.each { |v| v.separate_vg_name = nil }
-        end
-
-        context "and the system VG must be located in the boot disk" do
-          let(:expected_scenario_filename) { "agama-basis-lvm-single_disk" }
-
-          # Creates the boot partition and the system VG with all volumes at the target disk
-          include_examples "proposed layout"
-        end
-
-        context "and the system VG is allowed to use several disks" do
-          before { settings.candidate_devices = ["/dev/sdb", "/dev/sdc"] }
-
-          context "if all volumes fit when using only a disk" do
+          context "and the system VG must be located in the boot disk" do
             let(:expected_scenario_filename) { "agama-basis-lvm-single_disk" }
 
             # Creates the boot partition and the system VG with all volumes at the target disk
             include_examples "proposed layout"
           end
 
-          context "if several disks must be used for the volumes to fit" do
-            before do
-              root = settings.volumes.find { |v| v.mount_point == "/" }
-              root.min_size = Y2Storage::DiskSize.GiB(380)
-              root.max_size = Y2Storage::DiskSize.GiB(380)
+          context "and the system VG is allowed to use several disks" do
+            before { settings.candidate_devices = ["/dev/sdb", "/dev/sdc"] }
+
+            context "if all volumes fit when using only a disk" do
+              let(:expected_scenario_filename) { "agama-basis-lvm-single_disk" }
+
+              # Creates the boot partition and the system VG with all volumes at the target disk
+              include_examples "proposed layout"
             end
 
-            let(:expected_scenario_filename) { "agama-basis-lvm-several_disks" }
+            context "if several disks must be used for the volumes to fit" do
+              before do
+                root = settings.volumes.find { |v| v.mount_point == "/" }
+                root.min_size = Y2Storage::DiskSize.GiB(380)
+                root.max_size = Y2Storage::DiskSize.GiB(380)
+              end
 
-            # Creates the system VG with all volumes over the needed candidate disks
-            # FIXME: due to bsc#1211041, this resizes sdc1 to its minimum and consequently creates
-            # bigger LVs for swap, /srv or /home than the other LVM-based contexts
+              let(:expected_scenario_filename) { "agama-basis-lvm-several_disks" }
+
+              # Creates the system VG with all volumes over the needed candidate disks
+              # FIXME: due to bsc#1211041, this resizes sdc1 to its minimum and consequently creates
+              # bigger LVs for swap, /srv or /home than the other LVM-based contexts
+              include_examples "proposed layout"
+            end
+          end
+
+          context "and the system VG must be located in another disk (not the boot one)" do
+            before { settings.candidate_devices = ["/dev/sdc"] }
+
+            let(:expected_scenario_filename) { "agama-basis-lvm-separate_boot" }
+
+            # Creates the system VG at sdc and the partition needed for booting at sdb
             include_examples "proposed layout"
           end
         end
 
-        context "and the system VG must be located in another disk (not the boot one)" do
-          before { settings.candidate_devices = ["/dev/sdc"] }
+        context "if some volumes must be located in their own separate VG" do
+          before do
+            settings.volumes.each do |vol|
+              case vol.mount_point
+              when "/home"
+                vol.separate_vg_name = "home"
+                vol.device = "/dev/sda"
+              when "/srv"
+                vol.separate_vg_name = "srv"
+                vol.device = "/dev/sdc"
+              end
+            end
+          end
 
-          let(:expected_scenario_filename) { "agama-basis-lvm-separate_boot" }
+          let(:expected_scenario_filename) { "agama-basis-lvm-distributed" }
 
-          # Creates the system VG at sdc and the partition needed for booting at sdb
+          # Creates the system VG in the target disk and separate ones at sda and sdc as requested
+          include_examples "proposed layout"
+        end
+
+        context "if some volumes must be created at separate VG and others as partitions" do
+          before do
+            settings.volumes.each do |vol|
+              case vol.mount_point
+              when "/home"
+                vol.separate_vg_name = "home"
+                vol.device = "/dev/sda"
+              when "/srv"
+                vol.device = "/dev/sdc"
+              end
+            end
+          end
+
+          let(:expected_scenario_filename) { "agama-basis-lvm-distributed_with_partition" }
+
+          # Like the previous one but with /srv directly in a partition (no LVM)
           include_examples "proposed layout"
         end
       end
+    end
 
-      context "if some volumes must be located in their own separate VG" do
-        before do
-          settings.volumes.each do |vol|
-            case vol.mount_point
-            when "/home"
-              vol.separate_vg_name = "home"
-              vol.device = "/dev/sda"
-            when "/srv"
-              vol.separate_vg_name = "srv"
-              vol.device = "/dev/sdc"
-            end
-          end
-        end
+    context "with SpaceMaker strategy :auto set to resize" do
+      let(:proposal_settings) { { "windows_delete_mode" => :none } }
 
-        let(:expected_scenario_filename) { "agama-basis-lvm-distributed" }
+      include_examples "volume combinations"
+    end
 
-        # Creates the system VG in the target disk and separate ones at sda and sdc as requested
-        include_examples "proposed layout"
+    context "with SpaceMaker strategy :bigger_resize set to resize" do
+      let(:proposal_settings) { {} }
+
+      before do
+        settings.space_settings.strategy = :bigger_resize
+        settings.space_settings.actions = {
+          "/dev/sda1" => :resize,
+          "/dev/sdb1" => :resize,
+          "/dev/sdc1" => :resize
+        }
       end
 
-      context "if some volumes must be created at separate VG and others as partitions" do
-        before do
-          settings.volumes.each do |vol|
-            case vol.mount_point
-            when "/home"
-              vol.separate_vg_name = "home"
-              vol.device = "/dev/sda"
-            when "/srv"
-              vol.device = "/dev/sdc"
-            end
-          end
-        end
-
-        let(:expected_scenario_filename) { "agama-basis-lvm-distributed_with_partition" }
-
-        # Like the previous one but with /srv directly in a partition (no LVM)
-        include_examples "proposed layout"
-      end
+      include_examples "volume combinations"
     end
   end
 end
