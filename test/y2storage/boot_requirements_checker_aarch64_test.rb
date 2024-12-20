afc582c02a280d43e3d980e5d3a3603ef4f94279
Stefan Schubert <schubi@suse.de>
2024-12-20 15:02:04 +0100

    added bls to aarch64

--- test/y2storage/boot_requirements_checker_aarch64_test.rb
+++ test/y2storage/boot_requirements_checker_aarch64_test.rb
@@ -47,6 +47,23 @@ describe Y2Storage::BootRequirementsChecker do
     end
 
     include_context "plain UEFI"
-    include_examples "BLS bootloader"
+
+    context "when proposing a new EFI partition" do
+      let(:efi_part) { find_vol("/boot/efi", checker.needed_partitions(target)) }
+      let(:desired_efi_part) { find_vol("/boot/efi", checker.needed_partitions(:desired)) }
+      context "when proposing a new EFI partition" do
+        let(:efi_part) { find_vol("/boot/efi", checker.needed_partitions(target)) }
+        let(:desired_efi_part) { find_vol("/boot/efi", checker.needed_partitions(:desired)) }
+
+        include_examples "BLS bootloader not disabled"
+        context "and BLS installation is explicitly disabled" do
+          before do
+            allow(Y2Storage::StorageEnv.instance).to receive(:no_bls_bootloader).and_return(true)
+          end
+
+          include_examples "minimalistic EFI partition"
+        end
+      end
+    end
   end
 end
--- test/y2storage/boot_requirements_checker_x86_test.rb
+++ test/y2storage/boot_requirements_checker_x86_test.rb
@@ -297,7 +297,15 @@ describe Y2Storage::BootRequirementsChecker do
         let(:efi_partitions) { [] }
 
         include_examples "proposed EFI partition basics"
-        include_examples "BLS bootloader"
+        include_examples "BLS bootloader not disabled"
+
+        context "and BLS installation is explicitly disabled" do
+          before do
+            allow(Y2Storage::StorageEnv.instance).to receive(:no_bls_bootloader).and_return(true)
+          end
+
+          include_examples "flexible size EFI partition"
+        end
       end
     end
   end
