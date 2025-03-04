03bf2dd93d20c3e585f203a04d78481b8b87454e
Stefan Schubert <schubi@suse.de>
2025-03-04 13:08:02 +0100

    test

--- src/lib/y2storage/dialogs/proposal.rb
+++ src/lib/y2storage/dialogs/proposal.rb
@@ -191,7 +191,7 @@ module Y2Storage
             end
           end
         else
-          log.info "yyyyyyyyyyyyyyyyyyy #{devicegraph}"          
+          log.info "yyyyyyyyyyyyyyyyyyy #{devicegraph.inspect}"  
           use_tpm2 = storage_manager.encryption_use_tpm2
         end
         return "" if use_tpm2 == nil
