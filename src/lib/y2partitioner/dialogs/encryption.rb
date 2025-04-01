1c0c4a302c59451c66f80e02aa973b2177f0deb2
Stefan Schubert <schubi@suse.de>
2025-04-01 15:04:52 +0200

    test

--- src/lib/y2partitioner/dialogs/encryption.rb
+++ src/lib/y2partitioner/dialogs/encryption.rb
@@ -29,7 +29,6 @@ module Y2Partitioner
     # Part of {Actions::AddPartition} and {Actions::EditBlkDevice}.
     # Formerly MiniWorkflowStepPassword
     class Encryption < Base
-      include Yast::Logger
       # @param controller [Actions::Controllers::Filesystem]
       def initialize(controller)
         super()
@@ -98,10 +97,7 @@ module Y2Partitioner
 
         # @macro seeAbstractWidget
         def store
-          log.info "xxxxxxxxxxxxx"
-          log.info current_widget.inspect
           current_widget.store if current_widget.respond_to?(:store)
-          log.info "yyyyyyyyyyyy #{value}"                    
           @controller.action = value
         end
 
