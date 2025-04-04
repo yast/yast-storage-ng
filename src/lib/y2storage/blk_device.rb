8c5c6121dfbda4b5d7125998bf470dcc9ee5b6d4
Stefan Schubert <schubi@suse.de>
2025-04-04 08:42:58 +0200

    test

--- src/lib/y2storage/blk_device.rb
+++ src/lib/y2storage/blk_device.rb
@@ -329,7 +329,8 @@ module Y2Storage
     # @return [Encryption]
     def encrypt(method: EncryptionMethod::LUKS1, dm_name: nil, password: nil, **method_args)
       enc = encrypt_with_method(method, dm_name, **method_args)
-
+      log.info("xxxxxxblk #{enc}")
+log.info("xxxxxxblk #{enc.encryption_auth}")      
       enc.auto_dm_name = enc.dm_table_name.empty?
       enc.password = password if password
       enc.ensure_suitable_mount_by
