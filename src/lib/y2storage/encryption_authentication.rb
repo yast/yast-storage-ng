00a0df102b840d90c3d2a5680407c184dedc62bf
Stefan Schubert <schubi@suse.de>
2025-05-12 16:23:17 +0200

    rubocop

--- src/lib/y2storage/encryption_authentication.rb
+++ src/lib/y2storage/encryption_authentication.rb
@@ -77,7 +77,7 @@ module Y2Storage
                "<li><i>%{tpm2_and_pin}: </i>Like TPM2, but a password must be enter together.</li>" \
                "<li><i>%{fido2}: </i>External key device.</li>" \
                "</ul>", only_password: PASSWORD.name, tpm2: TPM2.name,
-               tpm2_and_pin: TPM2PIN.name, fido2: FIDO2.name))
+        tpm2_and_pin: TPM2PIN.name, fido2: FIDO2.name))
     end
 
     # @return [String] value
@@ -104,6 +104,5 @@ module Y2Storage
     def is?(*names)
       names.any? { |n| n.to_sym == to_sym }
     end
-
   end
 end
