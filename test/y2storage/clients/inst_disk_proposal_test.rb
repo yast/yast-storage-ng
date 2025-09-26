c68eb80b490b0652fa3d4929b461af9c1294f7fd
Stefan Schubert <schubi@suse.de>
2025-09-26 09:42:26 +0200

    fixed testcase

--- test/y2storage/clients/inst_disk_proposal_test.rb
+++ test/y2storage/clients/inst_disk_proposal_test.rb
@@ -524,7 +524,7 @@ describe Y2Storage::Clients::InstDiskProposal do
       end
 
       context "if the guided setup returns :next" do
-        let(:new_settings) { double("Y2Storage::ProposalSettings") }
+        let(:new_settings) { Y2Storage::ProposalSettings.new() }
 
         before do
           allow(proposal_dialog).to receive(:devicegraph).and_return(devicegraph)
